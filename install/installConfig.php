804d86e3a9426f8d7ac4eb653c4cbb7497ee54e8
Łukasz <lukasz.konczak@evolpe.pl>
2019-05-07 14:41:22 +0200

    User Story ref #62445: Dopasowanie instalatora do Minta Task ref #62446 IMPL: Dopasowanie instalatora do Minta

--- install/installConfig.php
+++ install/installConfig.php
@@ -509,22 +509,22 @@ EOQ;
 
 EOQ;
 
+  $out .=<<<EOQ3
+        </div>
+        </div>
 
-
+EOQ3;
         // ------------------
         //  Choose Demo Data
         // ------------------------->
 
 
-//         //demo data select
-//         $demoDD = "<select name='demoData' id='demoData' class='select'><option value='no' >".$mod_strings['LBL_NO']."</option><option value='yes'>".$mod_strings['LBL_YES']."</option>";
-//         $demoDD .= "</select>";
+        //demo data select
+        $demoDD = "<select name='demoData' id='demoData' class='select'><option value='no' >".$mod_strings['LBL_NO']."</option><option value='yes'>".$mod_strings['LBL_YES']."</option>";
+        $demoDD .= "</select>";
 
 //         $out .=<<<EOQ3
-//         </div>
-//         </div>
-
-//         <div class="floatbox full" id="fb0">
+//                <div class="floatbox full" id="fb0">
 //             <h2>{$mod_strings['LBL_MORE_OPTIONS_TITLE']}</h2>
 //         </div>
 
@@ -1635,7 +1635,7 @@ EOQ;
                                 postData += "&dbUSRData="+document.getElementById('dbUSRData').value;
                             }
 
-                            postData += "&demoData="+document.installForm.demoData.value;
+                            //postData += "&demoData="+document.installForm.demoData.value;
 
                             postData += "&to_pdf=1&sugar_body_only=1";
 
