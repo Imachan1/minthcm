eb3cf22fb9231c5d3ef79ac3d813981ff8a8fb2b
Łukasz <lukasz.konczak@evolpe.pl>
2019-05-08 08:09:16 +0200

    User Story ref #62445: Dopasowanie instalatora do Minta Task ref #62446 IMPL: Dopasowanie instalatora do Minta

--- install/install_utils.php
+++ install/install_utils.php
@@ -821,7 +821,7 @@ function handleSugarConfig() {
    $sugar_config['logger'] = array( 'level' => $setup_site_log_level,
       'file' => array(
          'ext' => '.log',
-         'name' => 'suitecrm',
+         'name' => 'minthcm',
          'dateFormat' => '%c',
          'maxSize' => '10MB',
          'maxLogs' => 10,
@@ -1044,7 +1044,7 @@ function handleWebConfig() {
    if ( empty($setup_site_log_file) ) {
       $setup_site_log_file = $sugar_config['log_file'];
       if ( empty($sugar_config['log_file']) ) {
-         $setup_site_log_file = 'suitecrm.log';
+         $setup_site_log_file = 'minthcm.log';
       }
    }
    if ( empty($setup_site_log_dir) ) {
@@ -1217,7 +1217,7 @@ function insert_default_settings() {
    if ( isset($_SESSION['smtp_from_addr']) && $_SESSION['smtp_from_addr'] ) {
       $fromAddress = $_SESSION['smtp_from_addr'];
    }
-   $fromName = 'SuiteCRM';
+   $fromName = 'MintCRM';
    if ( isset($_SESSION['smtp_from_name']) && $_SESSION['smtp_from_name'] ) {
       $fromName = $_SESSION['smtp_from_name'];
    }
