92d4e22ae8a1df2cd12906cfc2e291eecc312fa0
Dawid Brezwan <dawid.brezwan@evolpe.pl>
2019-05-27 09:12:57 +0200

    #62980: MintHCM - błędy i rzeczy do weryfikacji po review

--- include/language/en_us.lang.php
+++ include/language/en_us.lang.php
@@ -2073,6 +2073,11 @@ $app_strings = array(
 'LBL_RECRUITMENT' => 'Recruitment',
 'LBL_TIME_TRACKING' => 'Time Tracking',
 'LBL_ACTIVITIES' => 'Activities',
+    'LBL_NAV_RECRUITMENT_EB' => 'Recruitment/EB',
+    'LBL_NAV_TIME_TRACKING' => 'Time Tracking',
+    'LBL_NAV_COMPANY_LIFE' => 'Company Life',
+    'LBL_NAV_EMPLOYEE' => 'Employee',
+
 );
 
 $app_list_strings['moduleList']['Library'] = 'Library';
