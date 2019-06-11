2c81f61198af1df846ad4884001f59f11c840be4
Dawid Brezwan <dawid.brezwan@evolpe.pl>
2019-06-11 13:45:55 +0200

    ref #62339 Generowanie Onboardingu/Offboardingu ze złą datą rozpoczęcia

--- MintHCM/include/ScheduleGenerateOnboardingOffboarding/ScheduleGenerateOnboardingOffboarding.php
+++ MintHCM/include/ScheduleGenerateOnboardingOffboarding/ScheduleGenerateOnboardingOffboarding.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
  *
  * SugarCRM Community Edition is a customer relationship management program developed by
@@ -9,7 +8,7 @@
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
  * Copyright (C) 2011 - 2018 SalesAgility Ltd.
  *
- * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
+ * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM,
  * Copyright (C) 2018-2019 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
@@ -37,43 +36,48 @@
  * Section 5 of the GNU Affero General Public License version 3.
  *
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
- * these Appropriate Legal Notices must retain the display of the "Powered by SugarCRM" 
- * logo and "Supercharged by SuiteCRM" logo and "Reinvented by MintHCM" logo. 
- * If the display of the logos is not reasonably feasible for technical reasons, the 
- * Appropriate Legal Notices must display the words "Powered by SugarCRM" and 
+ * these Appropriate Legal Notices must retain the display of the "Powered by SugarCRM"
+ * logo and "Supercharged by SuiteCRM" logo and "Reinvented by MintHCM" logo.
+ * If the display of the logos is not reasonably feasible for technical reasons, the
+ * Appropriate Legal Notices must display the words "Powered by SugarCRM" and
  * "Supercharged by SuiteCRM" and "Reinvented by MintHCM".
  */
+class ScheduleGenerateOnboardingOffboarding
+{
+    protected $module_name;
+    protected $template_id;
+    protected $employee_id;
+    protected $date_start;
 
-class ScheduleGenerateOnboardingOffboarding {
+    public function __construct($module_name, $template_id, $employee_id,
+                                $date_start)
+    {
+        global $timedate;
+        $this->module_name = $module_name;
+        $this->template_id = $template_id;
+        $this->employee_id = $employee_id;
+        $this->date_start  = $date_start;
+        if (!empty($this->date_start)) {
+            $this->date_start = $timedate->to_db($this->date_start);
+        }
+    }
 
-   protected $module_name;
-   protected $template_id;
-   protected $employee_id;
-   protected $date_start;
-
-   public function __construct($module_name, $template_id, $employee_id, $date_start) {
-      $this->module_name = $module_name;
-      $this->template_id = $template_id;
-      $this->employee_id = $employee_id;
-      $this->date_start = $date_start;
-   }
-
-   public function schedule() {
-      global $current_user;
-      $jq = new SugarJobQueue();
-      $job = new SchedulersJob();
-      $job->name = "Schedule Generate Onboarding/Offboarding";
-      $job->target = "class::GenerateOnboardingOffboardingJob";
-      $data = base64_encode(json_encode(array(
-         'module_name' => $this->module_name,
-         'template_id' => $this->template_id,
-         'employee_id' => $this->employee_id,
-         'date_start' => $this->date_start,
-      )));
-      $job->data = $data;
-      $job->assigned_user_id = $current_user->id;
-      $jq->submitJob($job);
-      return true;
-   }
-
+    public function schedule()
+    {
+        global $current_user;
+        $jq                    = new SugarJobQueue();
+        $job                   = new SchedulersJob();
+        $job->name             = "Schedule Generate Onboarding/Offboarding";
+        $job->target           = "class::GenerateOnboardingOffboardingJob";
+        $data                  = base64_encode(json_encode(array(
+            'module_name' => $this->module_name,
+            'template_id' => $this->template_id,
+            'employee_id' => $this->employee_id,
+            'date_start' => $this->date_start,
+        )));
+        $job->data             = $data;
+        $job->assigned_user_id = $current_user->id;
+        $jq->submitJob($job);
+        return true;
+    }
 }
--- MintHCM/modules/OnboardingTemplates/GenerateOnboardingOffboarding.php
+++ MintHCM/modules/OnboardingTemplates/GenerateOnboardingOffboarding.php
@@ -1,6 +1,4 @@
 <?php
-
-
 /**
  *
  * SugarCRM Community Edition is a customer relationship management program developed by
@@ -43,144 +41,161 @@
  * Appropriate Legal Notices must display the words "Powered by SugarCRM" and 
  * "Supercharged by SuiteCRM" and "Reinvented by MintHCM".
  */
-
-if ( !defined('sugarEntry') ) {
-   define('sugarEntry', true);
+if (!defined('sugarEntry')) {
+    define('sugarEntry', true);
 }
 
 SugarAutoLoader::requireWithCustom('include/Notifications/Notification.php');
 
-class GenerateOnboardingOffboarding {
+class GenerateOnboardingOffboarding
+{
+    protected $module_name;
+    protected $template_id;
+    protected $employee_id;
+    protected $date_start;
+    protected $record_id;
+    protected $process;
+    protected $user_scheduled_onboarding;
 
-   protected $module_name;
-   protected $template_id;
-   protected $employee_id;
-   protected $date_start;
-   protected $record_id;
-   protected $process;
-   protected $user_scheduled_onboarding;
+    public function __construct($data)
+    {
+        $this->module_name = (isset($data['module_name'])) ? $data['module_name']
+                : null;
+        $this->template_id = (isset($data['template_id'])) ? $data['template_id']
+                : null;
+        $this->employee_id = (isset($data['employee_id'])) ? $data['employee_id']
+                : null;
+        $this->date_start  = (isset($data['date_start'])) ? $data['date_start'] : null;
+        $this->record_id   = (isset($data['record_id'])) ? $data['record_id'] : null;
+        if (isset($data['user_scheduled_onboarding_id'])) {
+            $this->user_scheduled_onboarding = BeanFactory::getBean('Users',
+                    $data['user_scheduled_onboarding_id']);
+        } else {
+            $this->user_scheduled_onboarding = BeanFactory::newBean('Users');
+        }
+    }
 
-   public function __construct($data) {
-      $this->module_name = (isset($data['module_name'])) ? $data['module_name'] : null;
-      $this->template_id = (isset($data['template_id'])) ? $data['template_id'] : null;
-      $this->employee_id = (isset($data['employee_id'])) ? $data['employee_id'] : null;
-      $this->date_start = (isset($data['date_start'])) ? $data['date_start'] : null;
-      $this->record_id = (isset($data['record_id'])) ? $data['record_id'] : null;
-      if ( isset($data['user_scheduled_onboarding_id']) ) {
-         $this->user_scheduled_onboarding = BeanFactory::getBean('Users', $data['user_scheduled_onboarding_id']);
-      } else {
-         $this->user_scheduled_onboarding = BeanFactory::newBean('Users');
-   }
-   }
+    public function generate()
+    {
+        $template = BeanFactory::getBean($this->module_name, $this->template_id);
+        if ($this->module_name == 'OnboardingTemplates') {
+            $this->createProcess('Onboardings', 'onboardingtemplate_id',
+                $template->assigned_user_id);
+        } else {
+            $this->createProcess('Offboardings', 'offboardingtemplate_id',
+                $template->assigned_user_id);
+        }
+        if ($template->load_relationship('elements')) {
+            foreach ($template->elements->getBeans() as $element) {
+                $this->createRecordOfElementType($element);
+            }
+        }
+        $this->addNotification();
+    }
 
-   public function generate() {
-      $template = BeanFactory::getBean($this->module_name, $this->template_id);
-      if ( $this->module_name == 'OnboardingTemplates' ) {
-         $this->createProcess('Onboardings', 'onboardingtemplate_id', $template->assigned_user_id);
-      } else {
-         $this->createProcess('Offboardings', 'offboardingtemplate_id', $template->assigned_user_id);
-      }
-      if ( $template->load_relationship('elements') ) {
-         foreach ( $template->elements->getBeans() as $element ) {
-            $this->createRecordOfElementType($element);
-         }
-      }
-      $this->addNotification();
-   }
+    protected function createProcess($process_name, $relate_id_field_name,
+                                     $assigned_user_id)
+    {
+        $bean                        = BeanFactory::newBean($process_name);
+        $bean->date_start            = $this->date_start;
+        $bean->employee_id           = $this->employee_id;
+        $bean->$relate_id_field_name = $this->template_id;
+        $bean->assigned_user_id      = $assigned_user_id;
+        $bean->save();
+        $this->addSecurityGroupToRecord($bean,
+            $this->user_scheduled_onboarding->getUserPrivateGroup());
+        $this->process               = $bean;
+    }
 
-   protected function createProcess($process_name, $relate_id_field_name, $assigned_user_id) {
-      $bean = BeanFactory::newBean($process_name);
-      $bean->date_start = $this->date_start;
-      $bean->employee_id = $this->employee_id;
-      $bean->$relate_id_field_name = $this->template_id;
-      $bean->assigned_user_id = $assigned_user_id;
-      $bean->save();
-      $this->addSecurityGroupToRecord($bean, $this->user_scheduled_onboarding->getUserPrivateGroup());
-      $this->process = $bean;
-   }
-
-   protected function addSecurityGroupToRecord($bean, $sg_id) {
-      $sg_relation_name = 'SecurityGroups';
-      if ( $bean->load_relationship($sg_relation_name) ) {
-         return $bean->$sg_relation_name->add($sg_id);
-      } else {
-         $GLOBALS['log']->fatal("Unable to load relationship {$sg_relation_name} for {$bean->object_name}");
-         return false;
-      }
-   }
-
-   protected function createRecordOfElementType($element) {
-      switch ( $element->type ) {
-         case 'task':
-            $bean = $this->createTask($element);
-            break;
-         case 'training':
-            $bean = $this->createTraining($element);
-            break;
-         case 'exit_interview':
-            $bean = $this->createExitInterview($element);
-            break;
-         default:
+    protected function addSecurityGroupToRecord($bean, $sg_id)
+    {
+        $sg_relation_name = 'SecurityGroups';
+        if ($bean->load_relationship($sg_relation_name)) {
+            return $bean->$sg_relation_name->add($sg_id);
+        } else {
+            $GLOBALS['log']->fatal("Unable to load relationship {$sg_relation_name} for {$bean->object_name}");
             return false;
-      }
-      return $this->addSecurityGroupToRecord($bean, $this->user_scheduled_onboarding->getUserPrivateGroup());
-   }
+        }
+    }
 
-   protected function createTask($element) {
-      global $timedate;
-      $bean = BeanFactory::newBean('Tasks');
-      $bean->name = $element->name;
-      $bean->assigned_user_id = (( bool ) $element->own_task ? $this->employee_id : $element->users_id);
-      $date_start_object = new DateTime($this->date_start);
-      $date_start_object->modify("+" . $element->days_from_start . " days");
-      $bean->date_start = $date_start_object->format($timedate->get_db_date_time_format());
-      $date_due = $date_start_object->modify("+" . $element->task_duration . " hours");
-      $bean->date_due = $date_due->format($timedate->get_db_date_time_format());
-      $bean->parent_type = $this->process->module_name;
-      $bean->parent_id = $this->process->id;
-      $bean->save();
-      return $bean;
-   }
+    protected function createRecordOfElementType($element)
+    {
+        switch ($element->type) {
+            case 'task':
+                $bean = $this->createTask($element);
+                break;
+            case 'training':
+                $bean = $this->createTraining($element);
+                break;
+            case 'exit_interview':
+                $bean = $this->createExitInterview($element);
+                break;
+            default:
+                return false;
+        }
+        return $this->addSecurityGroupToRecord($bean,
+                $this->user_scheduled_onboarding->getUserPrivateGroup());
+    }
 
-   protected function createTraining($element) {
-      global $timedate;
-      $bean = BeanFactory::newBean('Trainings');
-      $bean->name = $element->name;
-      $bean->assigned_user_id = $element->users_id;
-      $date_start_object = new DateTime($this->date_start);
-      $date_start_object->modify("+" . $element->days_from_start . " days");
-      $bean->date_start = $date_start_object->format($timedate->get_db_date_time_format());
-      $date_end = $date_start_object->modify("+" . $element->task_duration . " hours");
-      $bean->date_end = $date_end->format($timedate->get_db_date_time_format());
-      $bean->training_type = "internal";
-      $bean->parent_type = $this->process->module_name;
-      $bean->parent_id = $this->process->id;
-      $bean->save();
-      return $bean;
-   }
+    protected function createTask($element)
+    {
+        global $timedate;
+        $bean                   = BeanFactory::newBean('Tasks');
+        $bean->name             = $element->name;
+        $bean->assigned_user_id = ((bool) $element->own_task ? $this->employee_id
+                : $element->users_id);
+        $date_start_object      = new DateTime($this->date_start);
+        $date_start_object->modify("+".$element->days_from_start." days");
+        $bean->date_start       = $date_start_object->format($timedate->get_db_date_time_format());
+        $date_due               = $date_start_object->modify("+".$element->task_duration." hours");
+        $bean->date_due         = $date_due->format($timedate->get_db_date_time_format());
+        $bean->parent_type      = $this->process->module_name;
+        $bean->parent_id        = $this->process->id;
+        $bean->save();
+        return $bean;
+    }
 
-   protected function createExitInterview($element) {
-      global $timedate;
-      $bean = BeanFactory::newBean('ExitInterviews');
-      $bean->name = $element->name;
-      $bean->assigned_user_id = $element->users_id;
-      $bean->employee_id = $this->employee_id;
-      $date_start_object = new DateTime($this->date_start);
-      $date_start_object->modify("+" . $element->days_from_start . " days");
-      $bean->date_start = $date_start_object->format($timedate->get_db_date_time_format());
-      $date_end = $date_start_object->modify("+" . $element->task_duration . " hours");
-      $bean->date_end = $date_end->format($timedate->get_db_date_time_format());
-      $bean->save();
-      return $bean;
-   }
+    protected function createTraining($element)
+    {
+        global $timedate;
+        $bean                   = BeanFactory::newBean('Trainings');
+        $bean->name             = $element->name;
+        $bean->assigned_user_id = $element->users_id;
+        $date_start_object      = new DateTime($this->date_start);
+        $date_start_object->modify("+".$element->days_from_start." days");
+        $bean->date_start       = $date_start_object->format($timedate->get_db_date_time_format());
+        $date_end               = $date_start_object->modify("+".$element->task_duration." hours");
+        $bean->date_end         = $date_end->format($timedate->get_db_date_time_format());
+        $bean->training_type    = "internal";
+        $bean->parent_type      = $this->process->module_name;
+        $bean->parent_id        = $this->process->id;
+        $bean->save();
+        return $bean;
+    }
 
-   protected function addNotification() {
-      global $app_strings, $current_user;
-      $notification = new Notification();
-      $notification->setAssignedUserId($current_user->id);
-      $notification->setDescription(translate($app_strings['LBL_GENERATE_ONBOARDING_OFFBOARDING']));
-      $notification->disableUniqueValidation();
-      $notification->saveAsAlert();
-   }
+    protected function createExitInterview($element)
+    {
+        global $timedate;
+        $bean                   = BeanFactory::newBean('ExitInterviews');
+        $bean->name             = $element->name;
+        $bean->assigned_user_id = $element->users_id;
+        $bean->employee_id      = $this->employee_id;
+        $date_start_object      = new DateTime($this->date_start);
+        $date_start_object->modify("+".$element->days_from_start." days");
+        $bean->date_start       = $date_start_object->format($timedate->get_db_date_time_format());
+        $date_end               = $date_start_object->modify("+".$element->task_duration." hours");
+        $bean->date_end         = $date_end->format($timedate->get_db_date_time_format());
+        $bean->save();
+        return $bean;
+    }
 
+    protected function addNotification()
+    {
+        global $app_strings, $current_user;
+        $notification = new Notification();
+        $notification->setAssignedUserId($current_user->id);
+        $notification->setDescription(translate($app_strings['LBL_GENERATE_ONBOARDING_OFFBOARDING']));
+        $notification->disableUniqueValidation();
+        $notification->saveAsAlert();
+    }
 }
