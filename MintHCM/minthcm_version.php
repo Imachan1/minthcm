c996182246a94f026aa37d96a86fe641ea1beda6
Michał Nowacki <michal.nowacki@evolpe.pl>
2019-07-05 10:14:38 +0200

    wersja

--- MintHCM/minthcm_version.php
+++ MintHCM/minthcm_version.php
@@ -47,5 +47,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$minthcm_version      = '3.0.0';
-$minthcm_timestamp    = '2019-05-28-00:00:00';
+$minthcm_version      = '3.0.1';
+$minthcm_timestamp    = '2019-07-05-00:00:00';
