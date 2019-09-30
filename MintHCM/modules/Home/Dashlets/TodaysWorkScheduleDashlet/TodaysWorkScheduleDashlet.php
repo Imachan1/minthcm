ef8ed15a8dfb7e77f115cfe9d2c30a90d2774048
Michał Nowacki <michal.nowacki@evolpe.pl>
2019-09-30 10:14:48 +0200

    #63684 dodanie ikonek do dashletów

--- MintHCM/modules/Home/Dashlets/TodaysWorkScheduleDashlet/TodaysWorkScheduleDashlet.php
+++ MintHCM/modules/Home/Dashlets/TodaysWorkScheduleDashlet/TodaysWorkScheduleDashlet.php
@@ -1,52 +1,9 @@
 <?php
 
-
-/**
- *
- * SugarCRM Community Edition is a customer relationship management program developed by
- * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
- *
- * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
- * Copyright (C) 2011 - 2018 SalesAgility Ltd.
- *
- * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
- * Copyright (C) 2018-2019 MintHCM
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU Affero General Public License version 3 as published by the
- * Free Software Foundation with the addition of the following permission added
- * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
- * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
- * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
- * details.
- *
- * You should have received a copy of the GNU Affero General Public License along with
- * this program; if not, see http://www.gnu.org/licenses or write to the Free
- * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
- * 02110-1301 USA.
- *
- * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
- * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
- *
- * The interactive user interfaces in modified source and object code versions
- * of this program must display Appropriate Legal Notices, as required under
- * Section 5 of the GNU Affero General Public License version 3.
- *
- * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
- * these Appropriate Legal Notices must retain the display of the "Powered by SugarCRM" 
- * logo and "Supercharged by SuiteCRM" logo and "Reinvented by MintHCM" logo. 
- * If the display of the logos is not reasonably feasible for technical reasons, the 
- * Appropriate Legal Notices must display the words "Powered by SugarCRM" and 
- * "Supercharged by SuiteCRM" and "Reinvented by MintHCM".
- */
-
 if ( !defined('sugarEntry') || !sugarEntry ) {
    die('Not A Valid Entry Point');
 }
+
 
 require_once('include/Dashlets/Dashlet.php');
 
@@ -70,6 +27,7 @@ class TodaysWorkScheduleDashlet extends Dashlet {
       }
 
       parent::__construct($id); // call parent constructor
+      $this->seedBean = BeanFactory::newBean('WorkSchedules');
 
       $this->isConfigurable = true; // dashlet is configurable
       $this->isRefreshable = true;
@@ -96,7 +54,7 @@ class TodaysWorkScheduleDashlet extends Dashlet {
       $this->assignVariablesToSmartyForDisplay($ss);
       $str = $ss->fetch(get_custom_file_if_exists('modules/Home/Dashlets/TodaysWorkScheduleDashlet/TodaysWorkScheduleDashlet.tpl'));
       $lbl = isset($this->dashletStrings['LBL_DBLCLICK_HELP']) ?
-         $this->dashletStrings['LBL_DBLCLICK_HELP'] : '';
+              $this->dashletStrings['LBL_DBLCLICK_HELP'] : '';
       return parent::display($lbl) . $str . '<br />' . $this->processAutoRefresh() . '<br />'; // return parent::display for title and such
    }
 
@@ -111,6 +69,16 @@ class TodaysWorkScheduleDashlet extends Dashlet {
       $ss->assign('APP', $app_strings);
       $ss->assign('APPLIST', $app_list_strings);
       $ss->assign('MOD', $mod_strings);
+      include 'config_redmine.php';
+      if ( isset($config_redmine['redmine_rest']) && !empty($config_redmine['redmine_rest']) ) {
+         $redmineIssueUrl = ($config_redmine['redmine_rest_ssl'] == 'yes') ? 'https://' : 'http://';
+         $redmineIssueUrl .= $config_redmine['redmine_rest'];
+         if ( !empty($config_redmine["redmine_rest_port"]) ) {
+            $redmineIssueUrl .= ':' . $config_redmine["redmine_rest_port"];
+         }
+         $redmineIssueUrl .= '/issues/';
+         $ss->assign('redmineURL', $redmineIssueUrl);
+      }
    }
 
    /**
