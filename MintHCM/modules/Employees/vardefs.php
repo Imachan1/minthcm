a629deb8e09454ee75b307cd326026479b2a3d44
gawronekm <marcin.gawronek@evolpe.net>
2019-11-12 18:12:27 +0100

    Ref #66575 Enable Audit for Employee

--- MintHCM/modules/Employees/vardefs.php
+++ MintHCM/modules/Employees/vardefs.php
@@ -1,7 +1,7 @@
 <?php
 
-if ( !defined('sugarEntry') || !sugarEntry ) {
-   die('Not A Valid Entry Point');
+if (!defined('sugarEntry') || !sugarEntry) {
+    die('Not A Valid Entry Point');
 }
 /**
  *
@@ -11,7 +11,7 @@ if ( !defined('sugarEntry') || !sugarEntry ) {
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
  * Copyright (C) 2011 - 2018 SalesAgility Ltd.
  *
- * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
+ * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM,
  * Copyright (C) 2018-2019 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
@@ -39,15 +39,15 @@ if ( !defined('sugarEntry') || !sugarEntry ) {
  * Section 5 of the GNU Affero General Public License version 3.
  *
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
- * these Appropriate Legal Notices must retain the display of the "Powered by SugarCRM" 
- * logo and "Supercharged by SuiteCRM" logo and "Reinvented by MintHCM" logo. 
- * If the display of the logos is not reasonably feasible for technical reasons, the 
- * Appropriate Legal Notices must display the words "Powered by SugarCRM" and 
+ * these Appropriate Legal Notices must retain the display of the "Powered by SugarCRM"
+ * logo and "Supercharged by SuiteCRM" logo and "Reinvented by MintHCM" logo.
+ * If the display of the logos is not reasonably feasible for technical reasons, the
+ * Appropriate Legal Notices must display the words "Powered by SugarCRM" and
  * "Supercharged by SuiteCRM" and "Reinvented by MintHCM".
  */
 global $dictionary;
-if ( empty($dictionary['User']) ) {
-   include('modules/Users/vardefs.php');
+if (empty($dictionary['User'])) {
+    include 'modules/Users/vardefs.php';
 }
 $dictionary['Employee'] = $dictionary['User'];
 //users of employees modules are not allowed to change the employee/user status.
@@ -65,130 +65,151 @@ $dictionary['Employee']['fields']['email_addresses_primary']['required'] = false
 $dictionary['Employee']['fields']['status']['studio'] = false;
 $dictionary['Employee']['fields']['status']['required'] = false;
 
-
 $dictionary["Employee"]["fields"]["spenttime"] = array(
-   'name' => 'spenttime',
-   'type' => 'link',
-   'relationship' => 'spenttime_employees',
-   'source' => 'non-db',
-   'side' => 'right',
-   'vname' => 'LBL_USERS_SPENT_TIME_TITLE',
+    'name' => 'spenttime',
+    'type' => 'link',
+    'relationship' => 'spenttime_employees',
+    'source' => 'non-db',
+    'side' => 'right',
+    'vname' => 'LBL_USERS_SPENT_TIME_TITLE',
 );
 $dictionary["Employee"]["fields"]["contracts"] = array(
-   'name' => 'contracts',
-   'type' => 'link',
-   'relationship' => 'contracts_employee',
-   'source' => 'non-db',
-   'side' => 'right',
-   'vname' => 'LBL_CONTRACTS',
+    'name' => 'contracts',
+    'type' => 'link',
+    'relationship' => 'contracts_employee',
+    'source' => 'non-db',
+    'side' => 'right',
+    'vname' => 'LBL_CONTRACTS',
 );
 $dictionary["Employee"]["fields"]["resources"] = array(
-   'name' => 'resources',
-   'type' => 'link',
-   'relationship' => 'resources_employee',
-   'source' => 'non-db',
-   'side' => 'right',
-   'vname' => 'LBL_RESOURCES',
+    'name' => 'resources',
+    'type' => 'link',
+    'relationship' => 'resources_employee',
+    'source' => 'non-db',
+    'side' => 'right',
+    'vname' => 'LBL_RESOURCES',
 );
 $dictionary["Employee"]["fields"]["reservations"] = array(
-   'name' => 'reservations',
-   'type' => 'link',
-   'relationship' => 'reservations_employee',
-   'source' => 'non-db',
-   'side' => 'right',
-   'vname' => 'LBL_RESERVATIONS',
+    'name' => 'reservations',
+    'type' => 'link',
+    'relationship' => 'reservations_employee',
+    'source' => 'non-db',
+    'side' => 'right',
+    'vname' => 'LBL_RESERVATIONS',
 );
 $dictionary["Employee"]["fields"]["periodsofemployment"] = array(
-   'name' => 'periodsofemployment',
-   'type' => 'link',
-   'relationship' => 'periodsofemployment_employee',
-   'source' => 'non-db',
-   'side' => 'right',
-   'vname' => 'LBL_PERIODSOFEMPLOYMENT',
+    'name' => 'periodsofemployment',
+    'type' => 'link',
+    'relationship' => 'periodsofemployment_employee',
+    'source' => 'non-db',
+    'side' => 'right',
+    'vname' => 'LBL_PERIODSOFEMPLOYMENT',
 );
 
 $dictionary["Employee"]["fields"]["goals"] = array(
-   'name' => 'goals',
-   'type' => 'link',
-   'relationship' => 'goals_employee',
-   'source' => 'non-db',
-   'side' => 'right',
-   'vname' => 'LBL_GOALS',
+    'name' => 'goals',
+    'type' => 'link',
+    'relationship' => 'goals_employee',
+    'source' => 'non-db',
+    'side' => 'right',
+    'vname' => 'LBL_GOALS',
 );
 $dictionary["Employee"]["fields"]["appraisals"] = array(
-   'name' => 'appraisals',
-   'type' => 'link',
-   'relationship' => 'appraisals_employee',
-   'source' => 'non-db',
-   'side' => 'right',
-   'vname' => 'LBL_APPRAISALS',
+    'name' => 'appraisals',
+    'type' => 'link',
+    'relationship' => 'appraisals_employee',
+    'source' => 'non-db',
+    'side' => 'right',
+    'vname' => 'LBL_APPRAISALS',
 );
 $dictionary["Employee"]["fields"]["roles"] = array(
-   'name' => 'roles',
-   'type' => 'link',
-   'relationship' => 'roles_employees',
-   'source' => 'non-db',
-   'module' => 'EmployeeRoles',
-   'bean_name' => 'EmployeeRoles',
-   'vname' => 'LBL_ROLES',
+    'name' => 'roles',
+    'type' => 'link',
+    'relationship' => 'roles_employees',
+    'source' => 'non-db',
+    'module' => 'EmployeeRoles',
+    'bean_name' => 'EmployeeRoles',
+    'vname' => 'LBL_ROLES',
 );
 $dictionary["Employee"]["fields"]["benefits"] = array(
-   'name' => 'benefits',
-   'type' => 'link',
-   'relationship' => 'benefits_employees',
-   'source' => 'non-db',
-   'module' => 'Benefits',
-   'bean_name' => 'Benefits',
-   'vname' => 'LBL_RESPONSIBILITIES',
+    'name' => 'benefits',
+    'type' => 'link',
+    'relationship' => 'benefits_employees',
+    'source' => 'non-db',
+    'module' => 'Benefits',
+    'bean_name' => 'Benefits',
+    'vname' => 'LBL_RESPONSIBILITIES',
 );
 $dictionary["Employee"]["fields"]["onboardings"] = array(
-   'name' => 'onboardings',
-   'type' => 'link',
-   'relationship' => 'onboardings_employee',
-   'source' => 'non-db',
-   'side' => 'right',
-   'vname' => 'LBL_ONBOARDINGS',
+    'name' => 'onboardings',
+    'type' => 'link',
+    'relationship' => 'onboardings_employee',
+    'source' => 'non-db',
+    'side' => 'right',
+    'vname' => 'LBL_ONBOARDINGS',
 );
 $dictionary["Employee"]["fields"]["offboardings"] = array(
-   'name' => 'offboardings',
-   'type' => 'link',
-   'relationship' => 'offboardings_employee',
-   'source' => 'non-db',
-   'side' => 'right',
-   'vname' => 'LBL_OFFBOARDINGS',
+    'name' => 'offboardings',
+    'type' => 'link',
+    'relationship' => 'offboardings_employee',
+    'source' => 'non-db',
+    'side' => 'right',
+    'vname' => 'LBL_OFFBOARDINGS',
 );
 $dictionary["Employee"]["fields"]["competencyratings"] = array(
-   'name' => 'competencyratings',
-   'type' => 'link',
-   'relationship' => 'competencyratings_employee',
-   'module' => 'CompetencyRatings',
-   'bean_name' => 'CompetencyRatings',
-   'source' => 'non-db',
-   'vname' => 'LBL_COMPETENCYRATINGS',
+    'name' => 'competencyratings',
+    'type' => 'link',
+    'relationship' => 'competencyratings_employee',
+    'module' => 'CompetencyRatings',
+    'bean_name' => 'CompetencyRatings',
+    'source' => 'non-db',
+    'vname' => 'LBL_COMPETENCYRATINGS',
 );
 $dictionary["Employee"]["fields"]["organizationalunits"] = array(
-   'name' => 'organizationalunits',
-   'type' => 'link',
-   'relationship' => 'employees_organizationalunits',
-   'source' => 'non-db',
-   'module' => 'OrganizationalUnits',
-   'bean_name' => 'OrganizationalUnits',
-   'vname' => 'LBL_RELATIONSHIP_ORGANIZATIONALUNITS_NAME',
-   'side' => 'right',
+    'name' => 'organizationalunits',
+    'type' => 'link',
+    'relationship' => 'employees_organizationalunits',
+    'source' => 'non-db',
+    'module' => 'OrganizationalUnits',
+    'bean_name' => 'OrganizationalUnits',
+    'vname' => 'LBL_RELATIONSHIP_ORGANIZATIONALUNITS_NAME',
+    'side' => 'right',
 );
 $dictionary["Employee"]["fields"]["certificates"] = array(
-   'name' => 'certificates',
-   'type' => 'link',
-   'relationship' => 'certificates_employee',
-   'source' => 'non-db',
-   'side' => 'right',
-   'vname' => 'LBL_CERTIFICATES',
+    'name' => 'certificates',
+    'type' => 'link',
+    'relationship' => 'certificates_employee',
+    'source' => 'non-db',
+    'side' => 'right',
+    'vname' => 'LBL_CERTIFICATES',
 );
 $dictionary["Employee"]["fields"]["applications"] = array(
-   'name' => 'applications',
-   'type' => 'link',
-   'relationship' => 'applications_employee',
-   'source' => 'non-db',
-   'side' => 'right',
-   'vname' => 'LBL_APPLICATIONS_SUBPANEL',
+    'name' => 'applications',
+    'type' => 'link',
+    'relationship' => 'applications_employee',
+    'source' => 'non-db',
+    'side' => 'right',
+    'vname' => 'LBL_APPLICATIONS_SUBPANEL',
 );
+
+$dictionary["Employee"]["audited"] = true;
+$dictionary["Employee"]["fields"]["employee_status"]["audited"] = true;
+$dictionary["Employee"]["fields"]["first_name"]["audited"] = true;
+$dictionary["Employee"]["fields"]["last_name"]["audited"] = true;
+$dictionary["Employee"]["fields"]["position_name"]["audited"] = true;
+$dictionary["Employee"]["fields"]["phone_work"]["audited"] = true;
+$dictionary["Employee"]["fields"]["phone_mobile"]["audited"] = true;
+$dictionary["Employee"]["fields"]["phone_home"]["audited"] = true;
+$dictionary["Employee"]["fields"]["phone_fax"]["audited"] = true;
+$dictionary["Employee"]["fields"]["phone_other"]["audited"] = true;
+$dictionary["Employee"]["fields"]["organizationalunit_name"]["audited"] = true;
+$dictionary["Employee"]["fields"]["reports_to_name"]["audited"] = true;
+$dictionary["Employee"]["fields"]["messenger_type"]["audited"] = true;
+$dictionary["Employee"]["fields"]["messenger_id"]["audited"] = true;
+$dictionary["Employee"]["fields"]["address_street"]["audited"] = true;
+$dictionary["Employee"]["fields"]["address_city"]["audited"] = true;
+$dictionary["Employee"]["fields"]["address_state"]["audited"] = true;
+$dictionary["Employee"]["fields"]["address_country"]["audited"] = true;
+$dictionary["Employee"]["fields"]["address_postalcode"]["audited"] = true;
+$dictionary["Employee"]["fields"]["business_role"]["audited"] = false;
+$dictionary["Employee"]["fields"]["candidate_id"]["audited"] = false;
