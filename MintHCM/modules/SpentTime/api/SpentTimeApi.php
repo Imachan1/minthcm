bebaa2853a1b41b8f45d4f4f4074a9bed1e6e14a
Michał Dobrzykowski <michal.dobrzykowski@evolpe.pl>
2019-11-28 11:07:39 +0100

    ref #61467 Dodanie typu nieobecności - 'Choroba - opieka'

--- MintHCM/modules/SpentTime/api/SpentTimeApi.php
+++ MintHCM/modules/SpentTime/api/SpentTimeApi.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /**
  *
  * SugarCRM Community Edition is a customer relationship management program developed by
@@ -9,7 +8,7 @@
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
  * Copyright (C) 2011 - 2018 SalesAgility Ltd.
  *
- * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
+ * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM,
  * Copyright (C) 2018-2019 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
@@ -37,33 +36,35 @@
  * Section 5 of the GNU Affero General Public License version 3.
  *
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
- * these Appropriate Legal Notices must retain the display of the "Powered by SugarCRM" 
- * logo and "Supercharged by SuiteCRM" logo and "Reinvented by MintHCM" logo. 
- * If the display of the logos is not reasonably feasible for technical reasons, the 
- * Appropriate Legal Notices must display the words "Powered by SugarCRM" and 
+ * these Appropriate Legal Notices must retain the display of the "Powered by SugarCRM"
+ * logo and "Supercharged by SuiteCRM" logo and "Reinvented by MintHCM" logo.
+ * If the display of the logos is not reasonably feasible for technical reasons, the
+ * Appropriate Legal Notices must display the words "Powered by SugarCRM" and
  * "Supercharged by SuiteCRM" and "Reinvented by MintHCM".
  */
 
-class SpentTimeApi {
+class SpentTimeApi
+{
 
-   public function getCountOfSpendTimeRecordsInGivenFrame($id, $workschedule_id, $date_start, $date_end, $frontend = false) {
-      $frontend = !!$frontend;
-      if ( !empty($workschedule_id) && !empty($date_start) && !empty($date_end) ) {
-         if ( $frontend ) {
-            global $timedate;
-            $date_start = $timedate->to_db($date_start);
-            $date_end = $timedate->to_db($date_end);
-         }
-         if ( empty($date_start) || empty($date_end) ) {
-            $GLOBALS['log']->fatal('Empty date_start or date_end while validating spent time ' . $frontend ? 'FRONTEND' : 'BACKEND');
-            $GLOBALS['log']->fatal('Arguments: ' . print_r(func_get_args(), true));
-         } else {
-            $db = DBManagerFactory::getInstance();
-            $id_where = '';
-            if ( isset($id) ) {
-               $id_where = "st.id != '{$id}' AND ";
+    public function getCountOfSpendTimeRecordsInGivenFrame($id, $workschedule_id, $date_start, $date_end, $frontend = false)
+    {
+        $frontend = !!$frontend;
+        if (!empty($workschedule_id) && !empty($date_start) && !empty($date_end)) {
+            if ($frontend) {
+                global $timedate;
+                $date_start = $timedate->to_db($date_start);
+                $date_end = $timedate->to_db($date_end);
             }
-            $sql = "SELECT COUNT(st.id) AS count FROM workschedules_spenttime ws "
+            if (empty($date_start) || empty($date_end)) {
+                $GLOBALS['log']->fatal('Empty date_start or date_end while validating spent time ' . $frontend ? 'FRONTEND' : 'BACKEND');
+                $GLOBALS['log']->fatal('Arguments: ' . print_r(func_get_args(), true));
+            } else {
+                $db = DBManagerFactory::getInstance();
+                $id_where = '';
+                if (isset($id)) {
+                    $id_where = "st.id != '{$id}' AND ";
+                }
+                $sql = "SELECT COUNT(st.id) AS count FROM workschedules_spenttime ws "
                     . "LEFT JOIN spenttime st ON "
                     . "ws.spenttime_id=st.id "
                     . "AND ws.workschedule_id = '{$workschedule_id}' WHERE "
@@ -73,25 +74,27 @@ class SpentTimeApi {
                     . "(st.date_start < '{$date_end}' AND st.date_end >= '{$date_end}') OR "
                     . "(st.date_start < '{$date_start}' AND st.date_end > '{$date_start}') OR "
                     . "(st.date_start > '{$date_start}' AND st.date_end < '{$date_end}'))";
-            return intval($db->getOne($sql));
-         }
-      }
-      return 0;
-   }
+                return intval($db->getOne($sql));
+            }
+        }
+        return 0;
+    }
 
-   public function canLogToWorkOffSchedule($workschedule_id) {
-      global $db;
-      $result = true;
-      $work_off_types = array(
-         'holiday',
-         'sick',
-         'occasional_leave',
-         'leave_at_request',
-         'overtime',
-         'excused_absence',
-      );
-      if ( !empty($workschedule_id) ) {
-         $sql = "
+    public function canLogToWorkOffSchedule($workschedule_id)
+    {
+        global $db;
+        $result = true;
+        $work_off_types = array(
+            'holiday',
+            'sick',
+            'sick_care',
+            'occasional_leave',
+            'leave_at_request',
+            'overtime',
+            'excused_absence',
+        );
+        if (!empty($workschedule_id)) {
+            $sql = "
             SELECT
                type
             FROM
@@ -99,27 +102,29 @@ class SpentTimeApi {
             WHERE
                id = '{$workschedule_id}' AND
                deleted = 0";
-         $get_one = $db->getOne($sql);
-         if ( !empty($get_one) && in_array($get_one, $work_off_types) ) {
-            $result = false;
-         }
-      }
-      return $result;
-   }
+            $get_one = $db->getOne($sql);
+            if (!empty($get_one) && in_array($get_one, $work_off_types)) {
+                $result = false;
+            }
+        }
+        return $result;
+    }
 
-   public function canLogTimeToPast($args) {
-      SugarAutoLoader::requireWithCustom('modules/SpentTime/SpentTimeActionAccess.php');
-      $checker = new SpentTimeActionAccess();
-      $checker->setBean(BeanFactory::getBean('WorkSchedules', $args['workschedule_id']));
-      return $checker->checkAccess('add_past_time');
-   }
+    public function canLogTimeToPast($args)
+    {
+        SugarAutoLoader::requireWithCustom('modules/SpentTime/SpentTimeActionAccess.php');
+        $checker = new SpentTimeActionAccess();
+        $checker->setBean(BeanFactory::getBean('WorkSchedules', $args['workschedule_id']));
+        return $checker->checkAccess('add_past_time');
+    }
 
-   public function getCurrentUserId($args) {
-      global $current_user;
-      if ( !empty($current_user->id) ) {
-         return $current_user->id;
-      }
-      return false;
-   }
+    public function getCurrentUserId($args)
+    {
+        global $current_user;
+        if (!empty($current_user->id)) {
+            return $current_user->id;
+        }
+        return false;
+    }
 
 }
