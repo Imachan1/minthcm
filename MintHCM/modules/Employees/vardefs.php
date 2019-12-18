61020a5a6941affdec0948aa4be0f05382813072
Michał Dobrzykowski <michal.dobrzykowski@evolpe.pl>
2019-12-18 16:16:30 +0100

    ref #66870 Audyt pola E-mail w module Pracownicy

--- MintHCM/modules/Employees/vardefs.php
+++ MintHCM/modules/Employees/vardefs.php
@@ -222,4 +222,5 @@ $dictionary["Employee"]["fields"]["address_postalcode"]["audited"] = true;
 $dictionary["Employee"]["fields"]["business_role"]["audited"] = false;
 $dictionary["Employee"]["fields"]["candidate_id"]["audited"] = false;
 
+$dictionary['Employee']['fields']['email1']['audited'] = true;
 $dictionary['Employee']['fields']['email_addresses']['audited'] = true;
