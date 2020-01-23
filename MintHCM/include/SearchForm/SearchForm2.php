8ff24acb0d38c7670a4f8b24a1c5d3ffb97fb204
Michał Nowacki <michal.nowacki@evolpe.pl>
2020-01-23 09:36:15 +0100

    synch z oficialnym repo

--- MintHCM/include/SearchForm/SearchForm2.php
+++ MintHCM/include/SearchForm/SearchForm2.php
@@ -977,7 +977,7 @@ class SearchForm {
                      }
                   }
                } else {
-                $operator = !in_array($operator, ['subquery', 'subquery_with_in']) ? 'in' : $operator; //evolpe
+                $operator = !in_array($operator, ['subquery', 'subquery_with_in']) ? 'in' : $operator;
                   foreach ( $parms['value'] as $val ) {
                      if ( $val != ' ' && $val != '' ) {
                         if ( !empty($field_value) ) {
@@ -1192,7 +1192,7 @@ class SearchForm {
                   }
 
                   switch ( strtolower($operator) ) {
-                    case 'subquery_with_in': //evolpe
+                    case 'subquery_with_in':
                        $new_subquery = str_replace('{0}', $field_value, $parms['subquery']);
                        $where .= "{$db_field} IN ($new_subquery)";
                        break;
