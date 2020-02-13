a6b1c6978edc0abb0d048acfeefa8676bebfe1b5
Michał Nowacki <michal.nowacki@evolpe.pl>
2020-02-13 10:37:49 +0100

    ref #68576 Błąd podczas konwersji kandydata w pracownika

--- MintHCM/minthcm_version.php
+++ MintHCM/minthcm_version.php
@@ -46,5 +46,5 @@ if (!defined('sugarEntry') || !sugarEntry) {
     die('Not A Valid Entry Point');
 }
 
-$minthcm_version = '3.0.3';
-$minthcm_timestamp = '2020-01-21-00:00:00';
+$minthcm_version = '3.0.3.1';
+$minthcm_timestamp = '2020-02-13-00:00:00';
