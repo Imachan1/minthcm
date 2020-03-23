7c84972fa97b4a68b27c3b9425287235ba002473
gawronekm <marcin.gawronek@evolpe.net>
2020-03-23 07:04:14 +0100

    Ref #67873 Problem ze zmianą i usunięciem awatara

--- MintHCM/install/config/dashlets/pages_definitions.php
+++ MintHCM/install/config/dashlets/pages_definitions.php
@@ -8,7 +8,6 @@ $main = array(
                 2 => 'main_calendar_dashlet',
                 4 => 'main_leave_of_absence_dashlet',
                 6 => 'main_reservation_dashlet',
-
             ),
         ),
         1 => array(
