65f99d8ec25d853def8e69bb14cbb86797ed0de1
gawronekm <marcin.gawronek@evolpe.net>
2020-03-24 11:27:23 +0100

    podniesienie limitu

--- MintHCM/include/utils.php
+++ MintHCM/include/utils.php
@@ -4200,7 +4200,7 @@ function setPhpIniSettings()
     if (!empty($backtrack_limit)) {
         ini_set('pcre.backtrack_limit', '-1');
     }
-    ini_set('max_execution_time', '1200');
+    ini_set('max_execution_time', '3000');
     ini_set('memory_limit', '2048m');
 }
 
