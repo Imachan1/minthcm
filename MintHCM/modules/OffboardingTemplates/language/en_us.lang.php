7489678273fde613658b0cd16c90c3e52c407ca5
gawronekm <marcin.gawronek@evolpe.net>
2020-03-30 13:13:02 +0200

    Ref #69322 Szablony Onboarding / Offboarding - 1 szablon dla wielu stanowisk

--- MintHCM/modules/OffboardingTemplates/language/en_us.lang.php
+++ MintHCM/modules/OffboardingTemplates/language/en_us.lang.php
@@ -8,7 +8,7 @@
  * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
  * Copyright (C) 2011 - 2018 SalesAgility Ltd.
  *
- * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
+ * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM,
  * Copyright (C) 2018-2019 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
@@ -36,55 +36,55 @@
  * Section 5 of the GNU Affero General Public License version 3.
  *
  * In accordance with Section 7(b) of the GNU Affero General Public License version 3,
- * these Appropriate Legal Notices must retain the display of the "Powered by SugarCRM" 
- * logo and "Supercharged by SuiteCRM" logo and "Reinvented by MintHCM" logo. 
- * If the display of the logos is not reasonably feasible for technical reasons, the 
- * Appropriate Legal Notices must display the words "Powered by SugarCRM" and 
+ * these Appropriate Legal Notices must retain the display of the "Powered by SugarCRM"
+ * logo and "Supercharged by SuiteCRM" logo and "Reinvented by MintHCM" logo.
+ * If the display of the logos is not reasonably feasible for technical reasons, the
+ * Appropriate Legal Notices must display the words "Powered by SugarCRM" and
  * "Supercharged by SuiteCRM" and "Reinvented by MintHCM".
  */
 $mod_strings = array(
-   'LBL_ASSIGNED_TO_ID' => 'Assigned User Id',
-   'LBL_ASSIGNED_TO_NAME' => 'Assigned to',
-   'LBL_SECURITYGROUPS' => 'Security Groups',
-   'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => 'Security Groups',
-   'LBL_ID' => 'ID',
-   'LBL_DATE_ENTERED' => 'Date Created',
-   'LBL_DATE_MODIFIED' => 'Date Modified',
-   'LBL_MODIFIED' => 'Modified by',
-   'LBL_MODIFIED_NAME' => 'Modified by Name',
-   'LBL_CREATED' => 'Created by',
-   'LBL_DESCRIPTION' => 'Description',
-   'LBL_DELETED' => 'Deleted',
-   'LBL_NAME' => 'Name',
-   'LBL_CREATED_USER' => 'Created by User',
-   'LBL_MODIFIED_USER' => 'Modified by User',
-   'LBL_LIST_NAME' => 'Name',
-   'LBL_EDIT_BUTTON' => 'Edit',
-   'LBL_REMOVE' => 'Remove',
-   'LBL_ASCENDING' => 'Ascending',
-   'LBL_DESCENDING' => 'Descending',
-   'LBL_OPT_IN' => 'Opt In',
-   'LBL_OPT_IN_PENDING_EMAIL_NOT_SENT' => 'Pending Confirm opt in, Confirm opt in not sent',
-   'LBL_OPT_IN_PENDING_EMAIL_SENT' => 'Pending Confirm opt in, Confirm opt in sent',
-   'LBL_OPT_IN_CONFIRMED' => 'Opted in',
-   'LBL_LIST_FORM_TITLE' => 'Offboarding Templates List',
-   'LBL_MODULE_NAME' => 'Offboarding Templates',
-   'LBL_MODULE_TITLE' => 'Offboarding Templates',
-   'LBL_HOMEPAGE_TITLE' => 'My Offboarding Templates',
-   'LNK_NEW_RECORD' => 'Create Offboarding Template',
-   'LNK_LIST' => 'View Offboarding Templates',
-   'LNK_IMPORT_OFFBOARDINGTEMPLATES' => 'Import Offboarding Templates',
-   'LBL_SEARCH_FORM_TITLE' => 'Search Offboarding Templates',
-   'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
-   'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
-   'LBL_OFFBOARDINGTEMPLATES_SUBPANEL_TITLE' => 'Offboarding Templates',
-   'LBL_NEW_FORM_TITLE' => 'New Offboarding Template',
-   'LBL_OFFBOARDINGTEMPLATES_POSITIONS_TITLE' => 'Positions',
-   'LBL_POSITION_NAME' => 'Position',
-   'LBL_POSITION_ID' => 'Position (ID)',
-   'LBL_OFFBOARDINGS' => 'Offboardings',
-   'LBL_EMPLOYEE_NAME' => 'Employee',
-   'LBL_START_DATE' => 'Start Date',
-   'LBL_GENERATE_BUTTON' => 'Generate',
-   'LBL_ELEMENTS' => 'Onboarding/Offboarding Elements',
+    'LBL_ASSIGNED_TO_ID' => 'Assigned User Id',
+    'LBL_ASSIGNED_TO_NAME' => 'Assigned to',
+    'LBL_SECURITYGROUPS' => 'Security Groups',
+    'LBL_SECURITYGROUPS_SUBPANEL_TITLE' => 'Security Groups',
+    'LBL_ID' => 'ID',
+    'LBL_DATE_ENTERED' => 'Date Created',
+    'LBL_DATE_MODIFIED' => 'Date Modified',
+    'LBL_MODIFIED' => 'Modified by',
+    'LBL_MODIFIED_NAME' => 'Modified by Name',
+    'LBL_CREATED' => 'Created by',
+    'LBL_DESCRIPTION' => 'Description',
+    'LBL_DELETED' => 'Deleted',
+    'LBL_NAME' => 'Name',
+    'LBL_CREATED_USER' => 'Created by User',
+    'LBL_MODIFIED_USER' => 'Modified by User',
+    'LBL_LIST_NAME' => 'Name',
+    'LBL_EDIT_BUTTON' => 'Edit',
+    'LBL_REMOVE' => 'Remove',
+    'LBL_ASCENDING' => 'Ascending',
+    'LBL_DESCENDING' => 'Descending',
+    'LBL_OPT_IN' => 'Opt In',
+    'LBL_OPT_IN_PENDING_EMAIL_NOT_SENT' => 'Pending Confirm opt in, Confirm opt in not sent',
+    'LBL_OPT_IN_PENDING_EMAIL_SENT' => 'Pending Confirm opt in, Confirm opt in sent',
+    'LBL_OPT_IN_CONFIRMED' => 'Opted in',
+    'LBL_LIST_FORM_TITLE' => 'Offboarding Templates List',
+    'LBL_MODULE_NAME' => 'Offboarding Templates',
+    'LBL_MODULE_TITLE' => 'Offboarding Templates',
+    'LBL_HOMEPAGE_TITLE' => 'My Offboarding Templates',
+    'LNK_NEW_RECORD' => 'Create Offboarding Template',
+    'LNK_LIST' => 'View Offboarding Templates',
+    'LNK_IMPORT_OFFBOARDINGTEMPLATES' => 'Import Offboarding Templates',
+    'LBL_SEARCH_FORM_TITLE' => 'Search Offboarding Templates',
+    'LBL_HISTORY_SUBPANEL_TITLE' => 'View History',
+    'LBL_ACTIVITIES_SUBPANEL_TITLE' => 'Activities',
+    'LBL_OFFBOARDINGTEMPLATES_SUBPANEL_TITLE' => 'Offboarding Templates',
+    'LBL_NEW_FORM_TITLE' => 'New Offboarding Template',
+    'LBL_OFFBOARDINGTEMPLATES_POSITIONS_TITLE' => 'Positions',
+    'LBL_POSITION_NAME' => 'Position',
+    'LBL_POSITION_ID' => 'Position (ID)',
+    'LBL_OFFBOARDINGS' => 'Offboardings',
+    'LBL_EMPLOYEE_NAME' => 'Employee',
+    'LBL_START_DATE' => 'Start Date',
+    'LBL_GENERATE_BUTTON' => 'Generate',
+    'LBL_ELEMENTS' => 'Onboarding/Offboarding Elements',
 );
