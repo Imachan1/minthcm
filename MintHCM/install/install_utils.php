06b7404cb47a76eef7e58d390af5cb972e9bd490
gawronekm <marcin.gawronek@evolpe.net>
2020-03-31 11:41:19 +0200

    Zwiększenie czasu losowaego generowania dat

--- MintHCM/install/install_utils.php
+++ MintHCM/install/install_utils.php
@@ -2176,7 +2176,7 @@ function create_date($year = null, $mnth = null, $day = null) {
    global $timedate;
    $now = $timedate->getNow();
    if ( $day == null )
-      $day = $now->day + mt_rand(0, 30);
+      $day = $now->day + mt_rand(0, 100);
    return $timedate->asDbDate($now->get_day_begin($day, $mnth, $year));
 }
 
@@ -2200,7 +2200,7 @@ function create_time($hr = null, $min = null, $sec = null) {
 function create_past_date() {
    global $timedate;
    $now = $timedate->getNow(true);
-   $day = $now->day - mt_rand(1, 30);
+   $day = $now->day - mt_rand(1, 100);
    return $timedate->asDbDate($now->get_day_begin($day));
 }
 
