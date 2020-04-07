91ff0a4eccf6358223b4f6ab85a9a54ade2379fa
gawronekm <marcin.gawronek@evolpe.net>
2020-04-07 10:45:01 +0200

    quick fix

--- MintHCM/modules/OffboardingTemplates/controller.php
+++ MintHCM/modules/OffboardingTemplates/controller.php
@@ -41,7 +41,7 @@
  * Appropriate Legal Notices must display the words "Powered by SugarCRM" and
  * "Supercharged by SuiteCRM" and "Reinvented by MintHCM".
  */
-
+require_once 'modules/OnboardingTemplates/controller.php';
 class OffboardingTemplatesController extends OnboardingTemplatesController
 {
     const TEMPLATE = 'OffboardingTemplates';
