2aa1f6021d2e0d52881bceb03121ca336e0702b4
domagalskim <marek.domagalski@evolpe.pl>
2020-04-14 11:21:59 +0200

    ref #70022 Poprawki do generowania onboardingu/offboardingu

--- MintHCM/include/GenerateOnboardingOffboarding/generateOnboardingOffboarding.js
+++ MintHCM/include/GenerateOnboardingOffboarding/generateOnboardingOffboarding.js
@@ -7,7 +7,6 @@ generateOnboardingOffboarding = {
    relate_field_target_module: 'Employees',
    tpl: 'themes/SuiteP/tpls/generateOnboardingOffboarding.tpl',
    popup: null,
-   hide_dropdown = false,
    init: function () {
       viewTools.GUI.fieldErrorUnmark();
       if (this.popup) {
@@ -142,14 +141,14 @@ generateOnboardingOffboarding = {
       }
    },
    hideDropdown: function () {
-      let hide_dropdown = false;
+      let hide_dropdown = "";
       const module = $('input[name=module]:not(.form-control)').val();
       switch (module) {
          case 'Onboardings':
          case 'Offboardings':
          case 'OffboardingTemplates':
          case 'OnboardingTemplates':
-            hide_dropdown = true;
+            hide_dropdown = " hidden";
             break;
       }
       return hide_dropdown;
@@ -173,8 +172,7 @@ generateOnboardingOffboarding = {
       sqs_objects = [];
       let relate_field_name = this.relate_field_name + "_name";
       let relate_field_id = this.relate_field_name + "_id";
-      let hide_dropdown = this.hide_dropdown;
-      sqs_objects[this.form_name + "_" + this.relate_field_name + "_name"] = { form: this.form_name, method: "query", modules: [this.relate_field_target_module], group: "and", field_list: ["name", "id"], populate_list: [relate_field_name, relate_field_id, hide_dropdown], conditions: [{ "name": "name", "op": "like_custom", "end": "%", "value": "" }], required_list: [relate_field_id], order: "name", limit: "30", no_match_text: "Nie pasuje" };
+      sqs_objects[this.form_name + "_" + this.relate_field_name + "_name"] = { form: this.form_name, method: "query", modules: [this.relate_field_target_module], group: "and", field_list: ["name", "id"], populate_list: [relate_field_name, relate_field_id], conditions: [{ "name": "name", "op": "like_custom", "end": "%", "value": "" }], required_list: [relate_field_id], order: "name", limit: "30", no_match_text: "Nie pasuje" };
       sqs_objects[this.form_name + "_parent_name"] = { form: this.form_name, method: "query", modules: ["Accounts"], group: "or", field_list: ["name", "id"], populate_list: ["parent_name", "parent_id"], required_list: ["parent_id"], conditions: [{ "name": "name", "op": "like_custom", "end": "%", "value": "" }], order: "name", limit: "30", no_match_text: "Nie pasuje" };
       enableQS();
       if (typeof (changeParentQS) == 'undefined') {
