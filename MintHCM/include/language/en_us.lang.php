638d11c64192a696816e1d165e828d02bfe60272
gawronekm <marcin.gawronek@evolpe.net>
2020-05-07 10:41:48 +0200

    Ref #70395 Lista źródła pozyskania w kandydaturach do słownika

--- MintHCM/include/language/en_us.lang.php
+++ MintHCM/include/language/en_us.lang.php
@@ -4100,6 +4100,7 @@ $app_list_strings['list_type_list'] = array(
    '' => '',
    'Meetings-type' => 'Meetings - Type',
    'SpentTime-category' => 'Spent Time - Category',
+   'Candidatures-source' => 'Candidatures - Source',
 );
 
 $app_list_strings['reservations_parent_type_list'] = array(
