eac918aad5dd3ddc4821761456493d8b80ada336
gawronekm <marcin.gawronek@evolpe.net>
2020-07-02 15:32:21 +0200

    Ref #70409 Aktualności - zmiana popup ze sklejania newsów na walla

--- MintHCM/modules/News/js/mintNews.js
+++ MintHCM/modules/News/js/mintNews.js
@@ -3,15 +3,14 @@ mintNews = {
    id: "mintNews",
    news: [],
    init: function () {
-      var _this = this;
-      _this.loadNews(_this);
-      if (undefined === $('#' + _this.id).get(0) && _this.news.length > 0) {
-         $('body').append('<div id="' + _this.id + '">' + _this.getBody(_this.getHTMLFromNews(_this)) + '</div>');
+      this.loadNews();
+      if (undefined === $('#' + this.id).get(0) && this.news.length > 0) {
+         $('body').append('<div id="' + this.id + '">' + this.getBody(this.getHTMLFromNews()) + '</div>');
       }
       if ($('div.mintNews-boxes div.mintNews-announcement:visible').length === 0) {
          this.addCloseButton();
       }
-   },
+   }.bind(this),
    getBody: function (news) {
       var body = _.template('<div class="mintNews-container"><div class="mintNews-header"><%= title %></div><div class="mintNews-boxes"><%= news %></div></div>');
       return body({
@@ -19,25 +18,26 @@ mintNews = {
          news: news
       });
    },
-   loadNews: function (_this) {
+   loadNews: function () {
       viewTools.api.callCustomApi({
          module: 'UsersNews',
          action: 'getNewsForUser',
+         async: 'sync',
          callback: function (data) {
             if (!_.isEmpty(data)) {
                data.forEach(function (users_news) {
-                  _this.news.push(new News(users_news.news_type, users_news.id, users_news.name, users_news.content_of_announcement, viewTools.language.get('News', 'LBL_NEWS_' + users_news.news_type.toUpperCase() + '_BTN')));
+                  this.news.push(new News(users_news.news_type, users_news.id, users_news.name, users_news.content_of_announcement, viewTools.language.get('News', 'LBL_NEWS_' + users_news.news_type.toUpperCase() + '_BTN')));
                });
             }
-         }
+         }.bind(this)
       });
    },
-   getHTMLFromNews: function (_this) {
+   getHTMLFromNews: function () {
       var html = '';
-      _this.news.forEach(function (news_object) {
-         console.log(news_object.getBoxTemplate());
-         html += news_object.getBoxTemplate();
-      });
+      news_object.loadTpl();
+      this.news.forEach(function (news_object) {
+         html += news_object.getNewsBody();
+      }.bind(this));
       return html;
    },
    closeBox: function (record_id, type) {
@@ -62,7 +62,6 @@ mintNews = {
       $('#mintNews .mintNews-header').append('<span class="suitepicon suitepicon-action-clear" onclick="mintNews.close();"></span>');
    },
    close: function () {
-      var _this = this;
-      $('#' + _this.id).fadeOut();
-   }
+      $('#' + this.id).fadeOut();
+   }.bind(this)
 };
--- MintHCM/modules/News/js/news.js
+++ MintHCM/modules/News/js/news.js
@@ -1,7 +1,6 @@
 class News {
 
     news_tpl = 'themes/SuiteP/modules/News/tpls/News.tpl';
-
     constructor(type, record_id, name, content_of_announcement, button_text) {
         this.news_type = type;
         this.record_id = record_id;
@@ -10,13 +9,8 @@ class News {
         this.button_text = button_text;
     }
 
-    getBoxTemplate() {
-        debugger;
-        return this.getNewsBody();
-    }
-
     getNewsBody() {
-        var body = this.loadTpl(this.news_tpl);
+        var body = localStorage.getItem("news_template");
         return body({
             type: this.news_type,
             record_id: this.record_id,
@@ -26,15 +20,15 @@ class News {
         });
     }
 
-    loadTpl(tpl) {
+    loadTpl() {
         var template = '';
         $.ajax({
-            url: tpl,
+            url: this.tpl,
             success: function (result) {
                 template = result;
             },
             async: false,
-        });
-        return _.template(template);
+        }.bind(this));
+        localStorage.setItem("news_template", _.template(template));
     }
 }
