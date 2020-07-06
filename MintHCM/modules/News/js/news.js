2d7ee436ef01e3ecaf01f87ff2b36773501dbf0d
Łukasz <lukasz.konczak@evolpe.pl>
2020-07-06 09:06:44 +0200

    Ref #70409 Aktualności - zmiana popup ze sklejania newsów na walla

--- MintHCM/modules/News/js/news.js
+++ MintHCM/modules/News/js/news.js
@@ -1,6 +1,7 @@
 class News {
 
-    news_tpl = 'index.php?module=News&action=getNewsTpl';
+    news_tpl = 'themes/SuiteP/tpls/News.tpl';
+
     constructor(type, record_id, name, content_of_announcement, button_text) {
         this.news_type = type;
         this.record_id = record_id;
@@ -9,8 +10,12 @@ class News {
         this.button_text = button_text;
     }
 
+    getBoxTemplate() {
+        return this.getNewsBody();
+    }
+
     getNewsBody() {
-        var body = localStorage.getItem("news_template");
+        var body = this.loadTpl(this.news_tpl);
         return body({
             type: this.news_type,
             record_id: this.record_id,
@@ -20,15 +25,15 @@ class News {
         });
     }
 
-    loadTpl() {
+    loadTpl(tpl) {
         var template = '';
         $.ajax({
-            url: this.tpl,
+            url: tpl,
             success: function (result) {
                 template = result;
             },
             async: false,
-        }.bind(this));
-        localStorage.setItem("news_template", _.template(template));
+        });
+        return _.template(template);
     }
 }
