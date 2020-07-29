19900e7561343ed77e9d09299494d0251e1bc55b
gawronekm <marcin.gawronek@evolpe.net>
2020-07-29 10:11:58 +0200

    Ref #62360 Rekrutacje: Automatyczne uzupełnianie rekrutacji aktualnej w widoku QuickCreate kandydatury

---
+++ MintHCM/modules/Candidatures/js/view.quickcreate.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    fillCurrentRecruitment();
+});
+
+
+
+function fillCurrentRecruitment() {
+    var recruitment_name_selector = $('#recruitment_name');
+    var recruitment_id_selector = $('#recruitment_id');
+    var recruitment_end_name_selector = $('#recruitment_end_name');
+    var recruitment_end_id_selector = $('#recruitment_end_id');
+    if (recruitment_id_selector.val() && recruitment_name_selector.val() && !recruitment_end_name_selector.val()) {
+        viewTools.form.setValue(recruitment_end_name_selector, recruitment_name_selector.val());
+        viewTools.form.setValue(recruitment_end_id_selector, recruitment_id_selector.val());
+    }
+}
