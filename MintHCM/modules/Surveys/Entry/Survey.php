7c6ea3cb192a0a1a037a17b6d25b4fa036ee7a0c
Dominik Sucharski <dominik.sucharski@evolpe.pl>
2020-10-22 00:05:31 +0200

    no message

--- MintHCM/modules/Surveys/Entry/Survey.php
+++ MintHCM/modules/Surveys/Entry/Survey.php
@@ -77,8 +77,10 @@ EOF;
     <div class="container">
         <div class="row">
             <div class="col-md-offset-3 col-md-6">
-                <img src="<?php echo $companyLogoURL; ?>"/>
+                <div class="text-center">
+                    <img src="<?php echo $companyLogoURL ?>" style="max-width: 100%;"/>
             </div>
+        </div>
         </div>
         <div class="row well">
             <div class="col-md-offset-2 col-md-8">
