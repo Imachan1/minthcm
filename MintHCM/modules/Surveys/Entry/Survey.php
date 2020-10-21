68b9de145eeb9606b447f42416b54ecd4edab9fc
Dominik Sucharski <dominik.sucharski@evolpe.pl>
2020-10-22 00:05:31 +0200

    ref #74686 Dopasowanie logo w ankietach

--- MintHCM/modules/Surveys/Entry/Survey.php
+++ MintHCM/modules/Surveys/Entry/Survey.php
@@ -73,7 +73,9 @@ EOF;
     <div class="container">
         <div class="row">
             <div class="col-md-offset-3 col-md-6">
-                <img src="<?php echo $companyLogoURL ?>"/>
+                <div class="text-center">
+                    <img src="<?php echo $companyLogoURL ?>" style="max-width: 100%;"/>
+                </div>
             </div>
         </div>
         <div class="row well">
