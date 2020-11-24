bf6a749c6ddfce3d82f581b4fbbb25dc4fe82251
MarekLandzwojczak <marek.landzwojczak@evolpe.pl>
2020-11-24 13:52:31 +0100

    ref #75393 - [TECH] MintHCMPopup - loading screen Poprawa z CR

--- MintHCM/include/javascript/MintHCMPopup.js
+++ MintHCM/include/javascript/MintHCMPopup.js
@@ -1,15 +1,15 @@
 var MintHCMPopup = function (title, body, buttons, options, onShow) {
-    var classPopup = new MintHCMPopupClass(title,body,buttons,options,onShow);  
+    var classPopup = new MintHCMPopupClass(title, body, buttons, options, onShow);
     return classPopup.init();
 }
-MintHCMPopup.close = function() {
-    var id =$('.MintHCMPopup').attr('id')
-    $('#'+id).remove();
+MintHCMPopup.close = function () {
+    var id = $('.MintHCMPopup').attr('id')
+    $('#' + id).remove();
 };
 
-class MintHCMPopupClass{
+class MintHCMPopupClass {
 
-    constructor(title, body, buttons, options, onShow,load_id) {
+    constructor(title, body, buttons, options, onShow, load_id) {
         this.id = load_id || Date.now();
         this.title = title || "";
         this.body = body || "";
@@ -26,9 +26,9 @@ class MintHCMPopupClass{
             $('body').append('<div class="MintHCMPopup" id="' + this.id + '">' + this.getBody() + '</div>');
             if (typeof this.options.noCloseButton !== 'undefined' && this.options.noCloseButton) {
                 $('.MintHCMPopup-header').removeClass('MintHCMPopup-close');
-             } else {
-                $('#'+this.id +'_close').click(this.close);
-             }
+            } else {
+                $('#' + this.id + '_close').click(this.close);
+            }
             if (typeof this.options.css !== 'undefined' && this.options.css) {
                 $('#' + this.id + '> .MintHCMPopup - container').css(this.options.css);
             }
@@ -44,7 +44,7 @@ class MintHCMPopupClass{
                 title: this.title,
                 body: this.body,
                 buttons: this.getButtons(),
-                id:this.id
+                id: this.id
             });
         };
 
@@ -59,37 +59,37 @@ class MintHCMPopupClass{
 
         this.setButtonsEvents = function () {
             const _this = this;
-            $('#'+this.id+' .MintHCMPopup-buttons input').each(function (index) {
-                $(this).attr('id',_this.buttons[index].text );
+            $('#' + this.id + ' .MintHCMPopup-buttons input').each(function (index) {
+                $(this).attr('id', _this.buttons[index].text);
                 $(this).click(_this.buttons[index].click);
             });
         };
 
-      
+
 
         this.init();
         return this;
     }
     close() {
-        var new_id=this.id.replace("_close","_hidenId");
+        var new_id = this.id.replace("_close", "_hidenId");
         var id_close = document.getElementById(new_id).value;
-        $('#'+id_close).remove();
+        $('#' + id_close).remove();
     };
 
 };
 
 
-var showLoadingScreen = function (title,message) {
+var showLoadingScreen = function (title, message) {
     var loading;
-    if(message){
-        var body_html = "<div class='MintHCMPopup-load'><img src='themes/default/images/loading.gif' alt='loading'></img><div class='MintHCMPopup-load-message'><span>"+message+"</span></div></div>";
+    if (message) {
+        var body = "<div class='MintHCMPopup-load'><img src='themes/default/images/loading.gif' alt='loading'></img><div class='MintHCMPopup-load-message'><span>" + message + "</span></div></div>";
     }
-    else{
-        var body_html = "<div class='MintHCMPopup-load'><img src='themes/default/images/loading.gif' alt='loading'></img></div>";
+    else {
+        var body = "<div class='MintHCMPopup-load'><img src='themes/default/images/loading.gif' alt='loading'></img></div>";
     }
-    loading = new MintHCMPopupClass(title,body_html,'',{noCloseButton: true},'','load');   
+    loading = new MintHCMPopupClass(title, body, '', { noCloseButton: true }, '', 'load');
     loading.init();
-    return true;
+    return loading;
 };
 var closeLoadingScreen = function () {
     $('#load').remove();
