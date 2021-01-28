fe5d7c6f131e077137661be19185497037fcf9df
Marek Landzwojczak <marek.landzwojczak@evolpe.pl>
2021-01-28 12:01:58 +0100

    ref #75393 MintHCMPopup - loading screen

--- MintHCM/include/javascript/MintHCMPopup.js
+++ MintHCM/include/javascript/MintHCMPopup.js
@@ -3,7 +3,7 @@ var MintHCMPopup = function (title, body, buttons, options, onShow) {
     return classPopup.init();
 }
 MintHCMPopup.close = function () {
-    var id = $('.MintHCMPopup').attr('id')
+    var id = $( '.MintHCMPopup' ).last().attr( 'id' );
     $('#' + id).remove();
 };
 
@@ -20,49 +20,49 @@ class MintHCMPopupClass {
 
     init() {
         this.init = function () {
-            if (undefined !== $('#' + this.id).get(0)) {
-                $('#' + this.id).remove();
+            if ( undefined !== $( '#' + this.id ).get( 0 ) ) {
+                $( '#' + this.id ).remove();
             }
-            $('body').append('<div class="MintHCMPopup" id="' + this.id + '">' + this.getBody() + '</div>');
-            if (typeof this.options.noCloseButton !== 'undefined' && this.options.noCloseButton) {
-                $('.MintHCMPopup-header').removeClass('MintHCMPopup-close');
+            $( 'body' ).append( '<div class="MintHCMPopup" id="' + this.id + '">' + this.getBody() + '</div>' );
+            setTimeout( function () { //special timeout. Javasript has to wait on finishing append. So, does not remove the TimeOut (author; Michał Nowacki)
+                this.onShow();
+            }.bind(this), 0 );
+            if ( typeof this.options.noCloseButton !== 'undefined' && this.options.noCloseButton ) {
+                $( '.MintHCMPopup-header' ).removeClass( 'MintHCMPopup-close' );
             } else {
-                $('#' + this.id + '_close').click(this.close);
+                $( '#' + this.id + '_close' ).click( this.close );
             }
-            if (typeof this.options.css !== 'undefined' && this.options.css) {
-                $('#' + this.id + '> .MintHCMPopup - container').css(this.options.css);
+            if ( typeof this.options.css !== 'undefined' && this.options.css ) {
+                $( '#' + this.id + '> .MintHCMPopup-container' ).css( this.options.css );
             }
             this.setButtonsEvents();
-            if (this.onShow) {
-                this.onShow();
-            }
         };
 
         this.getBody = function () {
-            var body = _.template('<div class="MintHCMPopup-container"><div class="MintHCMPopup-header MintHCMPopup-close"><div class="MintHCMPopup-title"><input id="<%= id %>_hidenId" type="hidden" value="<%= id %>"></input><%= title %></div><span id="<%=id %>_close" class="suitepicon suitepicon-action-clear"></span><div style="clear: both;"></div></div><div class="MintHCMPopup-body"><%= body %></div><div class="MintHCMPopup-buttons"><%= buttons %></div></div>');
-            return body({
+            var body = _.template( '<div class="MintHCMPopup-container"><div class="MintHCMPopup-header MintHCMPopup-close"><div class="MintHCMPopup-title"><input id="<%= id %>_hidenId" type="hidden" value="<%= id %>"></input><%= title %></div><span id="<%=id %>_close" class="suitepicon suitepicon-action-clear"></span><div style="clear: both;"></div></div><div class="MintHCMPopup-body"><%= body %></div><div class="MintHCMPopup-buttons"><%= buttons %></div></div>' );
+            return body( {
                 title: this.title,
                 body: this.body,
                 buttons: this.getButtons(),
                 id: this.id
-            });
+            } );
         };
 
         this.getButtons = function () {
-            var button = _.template('<input type="button" value="<%= text %>" />');
+            var button = _.template( '<input type="button" value="<%= text %>" />' );
             var buttons = "";
-            this.buttons.forEach(function (btn) {
-                buttons += button({ text: btn.text });
-            });
+            this.buttons.forEach( function ( btn ) {
+                buttons += button( {text: btn.text} );
+            } );
             return buttons;
         };
 
         this.setButtonsEvents = function () {
             const _this = this;
-            $('#' + this.id + ' .MintHCMPopup-buttons input').each(function (index) {
-                $(this).attr('id', _this.buttons[index].text);
-                $(this).click(_this.buttons[index].click);
-            });
+            $( '#' + this.id + ' .MintHCMPopup-buttons input' ).each( function ( index ) {
+                $( this ).attr( 'id', _this.buttons[index].text );
+                $( this ).click( _this.buttons[index].click );
+            } );
         };
 
 
@@ -71,10 +71,13 @@ class MintHCMPopupClass {
         return this;
     }
     close() {
-        var new_id = this.id.replace("_close", "_hidenId");
-        var id_close = document.getElementById(new_id).value;
-        $('#' + id_close).remove();
-    };
+        var new_id = this.id.replace( "_close", "_hidenId" );
+        var id_close = document.getElementById( new_id ).value;
+        $( '#' + id_close ).fadeOut( 400, function () {
+            $( '#' + id_close ).remove();
+        } );
+    }
+    ;
 
 };
 
