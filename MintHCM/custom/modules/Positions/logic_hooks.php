5697024df94f3717742502c81b2b6bbaba580595
Szymon Kiszka <szymon.kiszka@evolpe.pl>
2021-06-29 14:28:26 +0200

    ref #86023 Stanowiska, jednostki organizacyjne - różne poprawki

---
+++ MintHCM/custom/modules/Positions/logic_hooks.php
@@ -0,0 +1,14 @@
+<?php
+// Do not store anything in this file that is not part of the array or the hook version.  This file will	
+// be automatically rebuilt in the future. 
+ $hook_version = 1; 
+if(!isset($hook_array)){ $hook_array = Array();} 
+// position, file, function 
+if(!isset($hook_array['before_save'])){ $hook_array['before_save'] = Array(); }
+$hook_array['before_save'][] = Array(1, 'Positions push feed', 'modules/Positions/SugarFeeds/PositionsFeed.php','PositionsFeed', 'pushFeed'); 
+
+if(!isset($hook_array['after_relationship_add'])){ $hook_array['after_relationship_add'] = Array(); }
+$hook_array['after_relationship_add'][] = Array(99, 'Add Unit to Employees', 'modules/Positions/logic_hooks/afterRelationshipAdd.php','afterRelationshipAdd', 'after_relationship_add'); 
+
+
+?>
--- MintHCM/modules/Users/js/view.edit.js
+++ MintHCM/modules/Users/js/view.edit.js
@@ -1,19 +1,8 @@
-$('#photo_file').change(function () {
-    $('#photo').val($('#photo_file').val().replace("C:\\fakepath\\", ""));
-});
-
-$(document).ready(function () {
-    $('#remove_button').prop("onclick", null).off("click");
-    $('#remove_button').click(function () {
-        window.onbeforeunload = null;
-        SUGAR.field.file.deleteAttachment("photo", "", this);
-    });
-});
-
 
 function getSupervisedUnitsIfEmployeeIsSupervisor() {
+    let status = $('select[name="status"] option:selected').val();
     let employee_status = $('select[name="employee_status"] option:selected').val();
-    if (employee_status != 'Active') {
+    if (employee_status != 'Active' || status != 'Active') {
         let employee_id = $('#record').val();
         viewTools.api.callCustomApi({
             module: 'Employees',
@@ -23,7 +12,8 @@ function getSupervisedUnitsIfEmployeeIsSupervisor() {
             callback: function (response) {
                 if (response != false) {
                     displayConfirmationWindow(response);
-                } else {
+                }
+                else {
                     SUGAR.ajaxUI.submitForm("EditView");
                     return false;
                 }
@@ -47,7 +37,7 @@ function displayConfirmationWindow(response) {
     else {
         dialog_div = $('<div>').css('display', 'none').html('The employee you want to deactivate is the current manager of the unit</br>' + units_links + 'In order to correctly generate the organizational structure of the department, a new manager should be indicated. Do you want to continue with deactivation?')
     }
-    
+
     dialog_div.dialog({
         resizable: false,
         height: 250,
@@ -55,14 +45,14 @@ function displayConfirmationWindow(response) {
         modal: true,
         buttons: [
             {
-                text: viewTools.language.get('Employees', 'LBL_EMPLOYEES_CONFIRMATION_BUTTON_CONFIRM'),
+                text: viewTools.language.get('Users', 'LBL_USERS_CONFIRMATION_BUTTON_CONFIRM'),
                 click: function () {
                     $(this).dialog("close");
-                    runEmployeesForceSave();
+                    runUsersForceSave();
                 },
             },
             {
-                text: viewTools.language.get('Employees', 'LBL_EMPLOYEES_CONFIRMATION_BUTTON_CANCEL'),
+                text: viewTools.language.get('Users', 'LBL_USERS_CONFIRMATION_BUTTON_CANCEL'),
                 click: function () {
                     $(this).dialog("close");
                 },
@@ -71,7 +61,7 @@ function displayConfirmationWindow(response) {
   });
 }
 
-function runEmployeesForceSave() {
+function runUsersForceSave() {
     SUGAR.ajaxUI.submitForm("EditView");
     return false;
 }
