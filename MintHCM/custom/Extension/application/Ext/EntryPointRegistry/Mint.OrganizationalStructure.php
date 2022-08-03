75cceb990cee8c3da7370da5ff65b6a5aaa68e0c
Eryk Andrzejewski <eryk.andrzejewski@evolpe.pl>
2022-08-03 10:14:09 +0200

    ref #102683 - Struktura organizacyjna - tryb pełnoekranowy i poprawa pierwszego ładowania

--- MintHCM/custom/Extension/application/Ext/EntryPointRegistry/Mint.OrganizationalStructure.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$entry_point_registry['OrganizationalStructure'] = array(
-    'file' => 'custom/include/OrganizationalStructure/OrganizationalStructureEntrypoint.php',
-    'auth' => true,
-);
