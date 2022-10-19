db67b4834665fec0e8e08e6ac50ceda009e56873
Eryk Andrzejewski <eryk.andrzejewski@evolpe.pl>
2022-10-19 13:43:19 +0200

    ref #96717 - Logowanie czasu pracy - ergonomia pracy

--- MintHCM/include/javascript/MintHCMDynamicPopupView.js
+++ MintHCM/include/javascript/MintHCMDynamicPopupView.js
@@ -72,11 +72,11 @@ var MintHCMDynamicPopupView = {
             let buttons =  [];
             if(!this.options.isDetailView){
                 buttons.push({
-                    text: viewTools.language.get('app_strings', 'LBL_SAVE_BUTTON_LABEL'), class: "primary", click: this.save.bind(this),
+                    text: viewTools.language.get('app_strings', 'LBL_SAVE_BUTTON_LABEL'), class: "primary", accesskey: "a", click: this.save.bind(this),
                  });
             }
             buttons.push({
-                text: viewTools.language.get('app_strings', 'LBL_CANCEL_BUTTON_LABEL'), class: "", click: this.close.bind(this)
+                text: viewTools.language.get('app_strings', 'LBL_CANCEL_BUTTON_LABEL'), class: "", accesskey: "l", click: this.close.bind(this)
              });
              if(this.options.isDetailView){
                 cont = cont.replace(/\#content /g,".MintHCMPopup-body ");
