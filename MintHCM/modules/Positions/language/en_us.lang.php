a40689f42664cabd7b1f0186a29e8e02ac82d84f
Szymon Maćkowiak <szymon.mackowiak@evolpe.pl>
2022-11-28 08:51:40 +0100

    ref#102685 Raporty zaawansowane i harmonogramy - poprawa etykiet

--- MintHCM/modules/Positions/language/en_us.lang.php
+++ MintHCM/modules/Positions/language/en_us.lang.php
@@ -126,4 +126,6 @@ $mod_strings = array(
     'LBL_TERMSOFEMPLOYMENT_SUBPANEL_TITLE' => 'Terms of Employment',
     'LBL_OFFBOARDINGTEMPLATE_NAME' => 'Offboarding Template Name',
     'LBL_ONBOARDINGTEMPLATE_NAME' => 'Onboarding Template Name',
+    'LBL_OFFBOARDINGTEMPLATE_ID' => 'Offboarding Template (ID)',
+    'LBL_ONBOARDINGTEMPLATE_ID' => 'Onboarding Template (ID)',
 );
