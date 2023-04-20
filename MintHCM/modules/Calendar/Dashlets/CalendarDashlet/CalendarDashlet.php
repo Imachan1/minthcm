c249f37852a2d61936c32ece26ebca5d83b18734
Michał Nowacki <michal.nowacki@evolpe.pl>
2023-04-20 14:42:54 +0200

    ref #84212 wyrzuciłem komentarze

--- MintHCM/modules/Calendar/Dashlets/CalendarDashlet/CalendarDashlet.php
+++ MintHCM/modules/Calendar/Dashlets/CalendarDashlet/CalendarDashlet.php
@@ -120,10 +120,7 @@ class CalendarDashlet extends Dashlet
         $cal_strings = return_module_language($current_language, 'Calendar');
 
         if (!ACLController::checkAccess('Calendar', 'list', true)) {
-            /* MintHCM #84212 START */
-            //ACLController::displayNoAccess(true);
             ACLController::displayNoAccess(false);
-            /* MintHCM #84212 END */  
         }
 
         $cal = new Calendar($this->view);
