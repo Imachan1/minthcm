40ba159eb54a76348ddf740954e75d78bb6adc0b
Łukasz Walczak <lukasz.walczak@evolpe.pl>
2023-05-11 10:58:03 +0200

    Init dla modulu - api

--- api/app/Controllers/Init/Languages.php
+++ api/app/Controllers/Init/Languages.php
@@ -1,28 +1,31 @@
 <?php
 
-namespace MintHCM\Api\Controllers\Actions;
+namespace MintHCM\Api\Controllers\Init;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Psr7\Response;
 
-class LanguagesAction
+class Languages
 {
 
     public function __invoke(Request $request, Response $response, array $args): Response
     {
+        global $sugar_config;
+
         $response = $response->withHeader('Content-type', 'application/json');
 
         $modules = $request->getAttribute('modules') ?? array();
-        $response_body = $this->getLanguages($modules);
+        $lang = $request->getAttribute('lang') ?? $_SESSION["authenticated_user_language"] ?? $sugar_config['default_language'];
+        $response_body = $this->getLanguages($modules, $lang);
 
         $response->getBody()->write(json_encode($response_body));
         return $response;
     }
 
-    public function getLanguages(array $modules = []): array
+    public function getLanguages(array $modules = [], string $lang = null): array
     {
         global $sugar_config, $app_list_strings, $app_strings, $current_language;
-        $current_language = $_SESSION["authenticated_user_language"] ?? $sugar_config['default_language'];
+        $current_language = $lang ?? $_SESSION["authenticated_user_language"] ?? $sugar_config['default_language'];
 
         $response = array();
         chdir('../legacy/');
