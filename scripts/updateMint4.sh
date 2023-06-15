922b5d304891c51f3e9ff598683923fa5df12f56
Michał Nowacki <michal.nowacki@evolpe.pl>
2023-06-15 09:58:16 +0200

    skrypt aktualizujący instancje Mint4

---
+++ scripts/updateMint4.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+repo="https://dev.evolpe.net/MintHCM/MintHCM.git"
+if [ "$#" -lt 1 ]; then
+        echo "Usage: $0 <instance name> [Git Branch] [Repo URL]"
+        exit 1
+fi
+instance_dir=${1}
+if [ "$#" -gt 1 ]; then
+        branch=${2}
+        shift
+fi
+if [ "$#" -gt 1 ]; then
+        repo=${2}
+        shift
+fi
+tmp_dir=$(mktemp -d -t minthcm-XXXXXXXXXX)
+if [ -z "$branch" ]; then
+        git clone ${repo} ${tmp_dir}
+else
+        git clone -b ${branch} ${repo} ${tmp_dir}
+fi
+
+echo "Aktualizacja custom\n"
+cp -r ${tmp_dir}/MintHCM/custom/* /var/www/${instance_dir}/legacy/custom/
+echo "Aktualizacja data\n"
+cp -r ${tmp_dir}/MintHCM/data/* /var/www/${instance_dir}/legacy/data/
+echo "aktualizacja include\n"
+cp -r ${tmp_dir}/MintHCM/include/* /var/www/${instance_dir}/legacy/include/
+echo "aktualizacja jssource\n"
+cp -r ${tmp_dir}/MintHCM/jssource/* /var/www/${instance_dir}/legacy/jssource/
+echo "aktualizacja metadata\n"
+cp -r ${tmp_dir}/MintHCM/metadata/* /var/www/${instance_dir}/legacy/metadata/
+echo "aktualizacja themes\n"
+cp -r ${tmp_dir}/MintHCM/themes/* /var/www/${instance_dir}/legacy/themes/
+echo "aktualizacja modules\n"
+cp -r ${tmp_dir}/MintHCM/modules/* /var/www/${instance_dir}/legacy/modules/
+echo "aktualizacja API\n"
+cp -r ${tmp_dir}/MintHCM/Api/* /var/www/${instance_dir}/legacy/Api/
+echo "aktualizacja vendor\n"
+cp -r ${tmp_dir}/MintHCM/vendor/* /var/www/${instance_dir}/legacy/vendor/
+echo "aktualizacja lib\n"
+cp -r ${tmp_dir}/MintHCM/lib/* /var/www/${instance_dir}/legacy/lib/
+echo "aktualizacja frontend\n"
+cp -r ${tmp_dir}/vue/dist/* /var/www/${instance_dir}/
+echo "aktualizacja api\n"
+cp -r ${tmp_dir}/api/* /var/www/${instance_dir}/api/
+
+echo "uprawnienia"
+chown -R www-data:www-data /var/www/${instance_dir}
+
+rm -rf ${tmp_dir}
