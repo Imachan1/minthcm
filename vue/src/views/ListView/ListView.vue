cf0d1b7c0486f52f5e39b793f8a60ac8adc746f7
Michał Trudziński <michal.trudzinski@evolpe.pl>
2023-06-16 17:33:12 +0200

    mint4 - fix nazwa modułu

--- vue/src/views/ListView/ListView.vue
+++ vue/src/views/ListView/ListView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="px-8 py-2">
-        <h1 v-text="module" />
+        <h1 v-text="moduleName" />
         <div class="elevation-4 mt-1 list-view">
             <ListViewFilters />
             <ListViewHeader />
@@ -15,12 +15,15 @@ import ListViewHeader from './ListViewHeader.vue'
 import ListViewTable from './ListViewTable.vue'
 import { useListViewStore } from './ListViewStore'
 import { useUrlStore } from '@/store/url'
+import { useLanguagesStore } from '@/store/languages'
 import ListViewFilters from './ListViewFilters.vue'
 
 const url = useUrlStore()
 const store = useListViewStore()
+const languages = useLanguagesStore()
 
 const module = computed(() => url.module)
+const moduleName = computed(() => languages.languages.app_list_strings?.moduleList?.[module.value])
 
 onMounted(async () => {
     if (store.module !== module.value) {
@@ -43,7 +46,6 @@ watch(module, (newVal, oldVal) => {
 
 <style scoped lang="scss">
 h1 {
-    color: rgb(var(--v-theme-secondary-dark));
     letter-spacing: 1px;
     font-weight: 600;
 }
