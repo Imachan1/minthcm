88e2f0167292c65db48513a782ea755437d82201
Michał Nowacki <michal.nowacki@evolpe.pl>
2023-06-20 13:48:26 +0200

    aktualizacja modułów do indeksacji

--- MintHCM/custom/modules/unified_search_modules_display.php
+++ MintHCM/custom/modules/unified_search_modules_display.php
@@ -1,82 +1,331 @@
 <?php
-// created: 2018-08-15 11:18:10
+// created: 2023-06-20 13:07:24
 $unified_search_modules_display = array(
-    'Accounts' => array(
+    'ACLRoles' => array(
         'visible' => true,
     ),
-    'Contacts' => array(
-        'visible' => true,
-    ),
-    'Opportunities' => array(
-        'visible' => true,
-    ),
-    'Calls' => array(
-        'visible' => true,
-    ),
-    'Documents' => array(
-        'visible' => true,
-    ),
-    'Cases' => array(
-        'visible' => true,
-    ),
-    'AOS_Contracts' => array(
+    'ACLActions' => array(
         'visible' => true,
     ),
     'Leads' => array(
         'visible' => true,
     ),
-    'Meetings' => array(
+    'Cases' => array(
         'visible' => true,
     ),
-    'Notes' => array(
+    'Bugs' => array(
+        'visible' => true,
+    ),
+    'ProspectLists' => array(
+        'visible' => true,
+    ),
+    'Prospects' => array(
+        'visible' => true,
+    ),
+    'Project' => array(
+        'visible' => true,
+    ),
+    'ProjectTask' => array(
         'visible' => true,
     ),
     'Campaigns' => array(
         'visible' => true,
     ),
+    'EmailMarketing' => array(
+        'visible' => true,
+    ),
+    'CampaignLog' => array(
+        'visible' => true,
+    ),
+    'CampaignTrackers' => array(
+        'visible' => true,
+    ),
+    'Releases' => array(
+        'visible' => true,
+    ),
+    'Groups' => array(
+        'visible' => true,
+    ),
+    'EmailMan' => array(
+        'visible' => true,
+    ),
+    'Schedulers' => array(
+        'visible' => true,
+    ),
+    'SchedulersJobs' => array(
+        'visible' => true,
+    ),
+    'Contacts' => array(
+        'visible' => true,
+    ),
+    'Accounts' => array(
+        'visible' => true,
+    ),
+    'DynamicFields' => array(
+        'visible' => true,
+    ),
+    'EditCustomFields' => array(
+        'visible' => true,
+    ),
+    'Opportunities' => array(
+        'visible' => true,
+    ),
+    'EmailTemplates' => array(
+        'visible' => true,
+    ),
+    'Notes' => array(
+        'visible' => true,
+    ),
+    'Calls' => array(
+        'visible' => true,
+    ),
+    'Emails' => array(
+        'visible' => true,
+    ),
+    'Meetings' => array(
+        'visible' => true,
+    ),
+    'Tasks' => array(
+        'visible' => true,
+    ),
+    'Users' => array(
+        'visible' => true,
+    ),
+    'Currencies' => array(
+        'visible' => true,
+    ),
+    'Trackers' => array(
+        'visible' => true,
+    ),
+    'Connectors' => array(
+        'visible' => true,
+    ),
+    'Import_1' => array(
+        'visible' => true,
+    ),
+    'Import_2' => array(
+        'visible' => true,
+    ),
+    'Versions' => array(
+        'visible' => true,
+    ),
+    'Administration' => array(
+        'visible' => true,
+    ),
+    'vCals' => array(
+        'visible' => true,
+    ),
+    'CustomFields' => array(
+        'visible' => true,
+    ),
+    'Alerts' => array(
+        'visible' => true,
+    ),
+    'Documents' => array(
+        'visible' => true,
+    ),
+    'DocumentRevisions' => array(
+        'visible' => true,
+    ),
+    'Roles' => array(
+        'visible' => true,
+    ),
+    'Audit' => array(
+        'visible' => true,
+    ),
+    'InboundEmail' => array(
+        'visible' => true,
+    ),
+    'SavedSearch' => array(
+        'visible' => true,
+    ),
+    'UserPreferences' => array(
+        'visible' => true,
+    ),
+    'MergeRecords' => array(
+        'visible' => true,
+    ),
+    'EmailAddresses' => array(
+        'visible' => true,
+    ),
+    'EmailText' => array(
+        'visible' => true,
+    ),
+    'Relationships' => array(
+        'visible' => true,
+    ),
     'Employees' => array(
         'visible' => true,
     ),
-    'Candidates' => array(
+    'Spots' => array(
         'visible' => true,
     ),
-    'Candidatures' => array(
+    'AOBH_BusinessHours' => array(
         'visible' => true,
     ),
-    'News' => array(
+    'SugarFeed' => array(
         'visible' => true,
     ),
-    'Ideas' => array(
+    'EAPM' => array(
         'visible' => true,
     ),
-    'Positions' => array(
+    'OAuthKeys' => array(
         'visible' => true,
     ),
-    'SalaryRanges' => array(
+    'OAuthTokens' => array(
         'visible' => true,
     ),
-    'Recruitments' => array(
+    'AM_ProjectTemplates' => array(
         'visible' => true,
     ),
-    'OrganizationalUnits' => array(
+    'AM_TaskTemplates' => array(
         'visible' => true,
     ),
-    'Competencies' => array(
+    'Favorites' => array(
         'visible' => true,
     ),
-    'CompetencyRatings' => array(
+    'AOK_Knowledge_Base_Categories' => array(
         'visible' => true,
     ),
-    'Appraisals' => array(
+    'AOK_KnowledgeBase' => array(
         'visible' => true,
     ),
-    'Benefits' => array(
+    'Reminders' => array(
         'visible' => true,
     ),
-    'Responsibilities' => array(
+    'Reminders_Invitees' => array(
         'visible' => true,
     ),
-    'TermsOfEmployment' => array(
+    'FP_events' => array(
+        'visible' => true,
+    ),
+    'FP_Event_Locations' => array(
+        'visible' => true,
+    ),
+    'AOD_IndexEvent' => array(
+        'visible' => true,
+    ),
+    'AOD_Index' => array(
+        'visible' => true,
+    ),
+    'AOP_Case_Events' => array(
+        'visible' => true,
+    ),
+    'AOP_Case_Updates' => array(
+        'visible' => true,
+    ),
+    'AOR_Reports' => array(
+        'visible' => true,
+    ),
+    'AOR_Fields' => array(
+        'visible' => true,
+    ),
+    'AOR_Charts' => array(
+        'visible' => true,
+    ),
+    'AOR_Conditions' => array(
+        'visible' => true,
+    ),
+    'AOR_Scheduled_Reports' => array(
+        'visible' => true,
+    ),
+    'AOS_Contracts' => array(
+        'visible' => true,
+    ),
+    'AOS_Invoices' => array(
+        'visible' => true,
+    ),
+    'AOS_PDF_Templates' => array(
+        'visible' => true,
+    ),
+    'AOS_Product_Categories' => array(
+        'visible' => true,
+    ),
+    'AOS_Products' => array(
+        'visible' => true,
+    ),
+    'AOS_Products_Quotes' => array(
+        'visible' => true,
+    ),
+    'AOS_Line_Item_Groups' => array(
+        'visible' => true,
+    ),
+    'AOS_Quotes' => array(
+        'visible' => true,
+    ),
+    'AOW_Actions' => array(
+        'visible' => true,
+    ),
+    'AOW_WorkFlow' => array(
+        'visible' => true,
+    ),
+    'AOW_Processed' => array(
+        'visible' => true,
+    ),
+    'AOW_Conditions' => array(
+        'visible' => true,
+    ),
+    'jjwg_Maps' => array(
+        'visible' => true,
+    ),
+    'jjwg_Markers' => array(
+        'visible' => true,
+    ),
+    'jjwg_Areas' => array(
+        'visible' => true,
+    ),
+    'jjwg_Address_Cache' => array(
+        'visible' => true,
+    ),
+    'Calls_Reschedule' => array(
+        'visible' => true,
+    ),
+    'SecurityGroups' => array(
+        'visible' => true,
+    ),
+    'OutboundEmailAccounts' => array(
+        'visible' => true,
+    ),
+    'TemplateSectionLine' => array(
+        'visible' => true,
+    ),
+    'OAuth2Tokens' => array(
+        'visible' => true,
+    ),
+    'OAuth2Clients' => array(
+        'visible' => true,
+    ),
+    'SurveyResponses' => array(
+        'visible' => true,
+    ),
+    'Surveys' => array(
+        'visible' => true,
+    ),
+    'SurveyQuestionResponses' => array(
+        'visible' => true,
+    ),
+    'SurveyQuestions' => array(
+        'visible' => true,
+    ),
+    'SurveyQuestionOptions' => array(
+        'visible' => true,
+    ),
+    'Skills' => array(
+        'visible' => true,
+    ),
+    'Attitudes' => array(
+        'visible' => true,
+    ),
+    'Knowledge' => array(
+        'visible' => true,
+    ),
+    'ReservationsCalendar' => array(
+        'visible' => true,
+    ),
+    'ResponsibilityActivities' => array(
+        'visible' => true,
+    ),
+    'Improvements' => array(
         'visible' => true,
     ),
     'Conclusions' => array(
@@ -85,85 +334,176 @@ $unified_search_modules_display = array(
     'Problems' => array(
         'visible' => true,
     ),
-    'Improvements' => array(
+    'Candidates' => array(
         'visible' => true,
     ),
-    'Applications' => array(
+    'Candidatures' => array(
         'visible' => true,
     ),
-    'Certificates' => array(
+    'Costs' => array(
+        'visible' => true,
+    ),
+    'Delegations' => array(
+        'visible' => true,
+    ),
+    'DelegationsLocale' => array(
+        'visible' => true,
+    ),
+    'Ideas' => array(
+        'visible' => true,
+    ),
+    'Rooms' => array(
+        'visible' => true,
+    ),
+    'Allocations' => array(
+        'visible' => true,
+    ),
+    'Workplaces' => array(
+        'visible' => true,
+    ),
+    'EmployeeInteractionTracking' => array(
+        'visible' => true,
+    ),
+    'Positions' => array(
+        'visible' => true,
+    ),
+    'Recruitments' => array(
+        'visible' => true,
+    ),
+    'Transportations' => array(
+        'visible' => true,
+    ),
+    'EmployeeRoles' => array(
+        'visible' => true,
+    ),
+    'Benefits' => array(
+        'visible' => true,
+    ),
+    'Responsibilities' => array(
+        'visible' => true,
+    ),
+    'SpentTime' => array(
+        'visible' => true,
+    ),
+    'WorkSchedules' => array(
+        'visible' => true,
+    ),
+    'WorkingMonths' => array(
+        'visible' => true,
+    ),
+    'NonWorkingDays' => array(
+        'visible' => true,
+    ),
+    'Resources' => array(
+        'visible' => true,
+    ),
+    'PeriodsOfEmployment' => array(
+        'visible' => true,
+    ),
+    'Reservations' => array(
+        'visible' => true,
+    ),
+    'Contracts' => array(
+        'visible' => true,
+    ),
+    'TermsOfEmployment' => array(
+        'visible' => true,
+    ),
+    'Trainings' => array(
+        'visible' => true,
+    ),
+    'OnboardingOffboardingElements' => array(
+        'visible' => true,
+    ),
+    'OnboardingTemplates' => array(
+        'visible' => true,
+    ),
+    'OffboardingTemplates' => array(
         'visible' => true,
     ),
     'ExitInterviews' => array(
         'visible' => true,
     ),
-    'AOP_Case_Events' => array(
-        'visible' => false,
-    ),
-    'AOP_Case_Updates' => array(
-        'visible' => false,
-    ),
-    'AOR_Reports' => array(
-        'visible' => false,
-    ),
-    'AOS_Invoices' => array(
-        'visible' => false,
-    ),
-    'AOS_PDF_Templates' => array(
-        'visible' => false,
-    ),
-    'AOS_Product_Categories' => array(
-        'visible' => false,
-    ),
-    'AOS_Products' => array(
-        'visible' => false,
-    ),
-    'AOS_Quotes' => array(
-        'visible' => false,
-    ),
-    'AOW_Processed' => array(
-        'visible' => false,
-    ),
-    'AOW_WorkFlow' => array(
-        'visible' => false,
-    ),
-    'Bugs' => array(
-        'visible' => false,
-    ),
-    'Calls_Reschedule' => array(
-        'visible' => false,
-    ),
-    'FP_Event_Locations' => array(
-        'visible' => false,
-    ),
-    'FP_events' => array(
-        'visible' => false,
-    ),
-    'Project' => array(
-        'visible' => false,
-    ),
-    'ProjectTask' => array(
-        'visible' => false,
-    ),
-    'ProspectLists' => array(
-        'visible' => false,
-    ),
-    'Prospects' => array(
-        'visible' => false,
-    ),
-    'Tasks' => array(
+    'Goals' => array(
         'visible' => true,
     ),
-    'jjwg_Address_Cache' => array(
-        'visible' => false,
+    'Appraisals' => array(
+        'visible' => true,
     ),
-    'jjwg_Areas' => array(
-        'visible' => false,
+    'AppraisalItems' => array(
+        'visible' => true,
     ),
-    'jjwg_Maps' => array(
-        'visible' => false,
+    'Onboardings' => array(
+        'visible' => true,
     ),
-    'jjwg_Markers' => array(
-        'visible' => false,
+    'Offboardings' => array(
+        'visible' => true,
     ),
+    'CompetencyRatings' => array(
+        'visible' => true,
+    ),
+    'Competencies' => array(
+        'visible' => true,
+    ),
+    'CareerPaths' => array(
+        'visible' => true,
+    ),
+    'News' => array(
+        'visible' => true,
+    ),
+    'UsersNews' => array(
+        'visible' => true,
+    ),
+    'Certificates' => array(
+        'visible' => true,
+    ),
+    'Applications' => array(
+        'visible' => true,
+    ),
+    'DashboardBackups' => array(
+        'visible' => true,
+    ),
+    'DashboardManager' => array(
+        'visible' => true,
+    ),
+    'DashboardHistory' => array(
+        'visible' => true,
+    ),
+    'KReports' => array(
+        'visible' => true,
+    ),
+    'ScheduleReports' => array(
+        'visible' => true,
+    ),
+    'ScheduleReportsLogs' => array(
+        'visible' => true,
+    ),
+    'KTemplates' => array(
+        'visible' => true,
+    ),
+    'PDFGenerator' => array(
+        'visible' => true,
+    ),
+    'PDFTemplates' => array(
+        'visible' => true,
+    ),
+    'SalaryRanges' => array(
+        'visible' => true,
+    ),
+    'EmployeeCertificates' => array(
+        'visible' => true,
+    ),
+    'Dictionaries' => array(
+        'visible' => true,
+    ),
+    'Comments' => array(
+        'visible' => true,
+    ),
+    'Reactions' => array(
+        'visible' => true,
+    ),
+    'Files' => array(
+        'visible' => true,
+    ),
+
 );
