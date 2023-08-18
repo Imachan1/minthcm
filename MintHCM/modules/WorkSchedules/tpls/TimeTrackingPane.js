40ffc0f2999ccad1c7c16a1794c83e84b3e328e3
Michał Nowacki <michal.nowacki@evolpe.pl>
2023-08-18 13:41:38 +0200

    Revert "Wyświetlanie nazwy zagadnienia w tooltipie zalogowanego czasu"
    This reverts commit c238e4addfb8bfe30fc275d15e5885cf494cb271.

--- MintHCM/modules/WorkSchedules/tpls/TimeTrackingPane.js
+++ MintHCM/modules/WorkSchedules/tpls/TimeTrackingPane.js
@@ -257,9 +257,9 @@ if (!window.TimePanel) { // avoid multi-declaration
                 /* MintHCM #93842 END */
                 var task_number_desc = '';
                 if (i.spendtime_projecttask_id != undefined && i.spendtime_projecttask_id != '') {
-                    task_number_desc = ' #' + i.spendtime_projecttask_id + ' ' + i.spendtime_projecttask_name;
+                    task_number_desc = ' #' + i.spendtime_projecttask_id;
                 }
-                i.$el.attr('title', toSugarTime(start) + ' - ' + toSugarTime(end) + task_number_desc + "\n> " + i.description);
+                i.$el.attr('title', toSugarTime(start) + ' - ' + toSugarTime(end) + task_number_desc + ' ' + i.description);
                 parent_el.append(i.$el);
             }
 
