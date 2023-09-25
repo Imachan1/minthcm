896d1a642e9b563ace9acd255ead1a81e8de30b5
Eryk Andrzejewski <eryk.andrzejewski@evolpe.pl>
2023-09-25 11:50:01 +0200

    ref #122601 - Filtrowanie po polach liczbowych na listach

--- vue/src/views/ListView/fields/numeric.vue
+++ vue/src/views/ListView/fields/numeric.vue
@@ -11,14 +11,23 @@
 
 <script setup lang="ts">
 import { ref, defineProps, defineEmits } from 'vue'
+import { useAuthStore } from '@/store/auth'
 
 const emit = defineEmits(['update:modelValue'])
 const props = defineProps(['input'])
 const value = ref(props.input?.value)
 const debounceTimeout = ref<number | null>(null)
 
+const auth = useAuthStore()
+
 function convertValue(v: string) {
-    return parseFloat(v)
+    return parseFloat(removeSeparators(v))
+}
+
+function removeSeparators(v: string): string {
+    const group_separator = auth.user.preferences.num_grp_sep
+    const without_spaces = v.replace(/\s+/g, '')
+    return group_separator ? without_spaces.replaceAll(group_separator, '') : without_spaces
 }
 
 function handleInput() {
