c41ba258c8bfd75521a676c5f99e5f64d394449a
Michał Nowacki <michal.nowacki@evolpe.pl>
2023-12-07 13:58:11 +0100

    poprawa pliku htaccess

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+if(!file_exists('.htaccess')) {
+  $htaccess_body = "<IfModule mod_rewrite.c>
+  Options +SymLinksIfOwnerMatch
+  RewriteEngine On
+
+  RewriteRule ^api/(.*?)$ install/index.php [L]
+
+  RewriteRule ^assets/(.*?)$ assets/$1 [L]
+  RewriteRule ^favicon.ico$ favicon.ico [L]
+  RewriteRule ^bg.jpg$ bg.jpg [L]
+</IfModule>";
+  file_put_contents('.htaccess', $htaccess_body);
+  
+}
+unlink('index.php');
+copy('vue/dist/index.html', 'index.html');
+header("Refresh:0");
