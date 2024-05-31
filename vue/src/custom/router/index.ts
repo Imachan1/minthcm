24ef8f95e4c6cb22e40a978dee71ff4806c1ff86
Michał Nowacki <michal.nowacki@evolpe.pl>
2024-05-31 13:07:22 +0200

    dodanie plików od maskarady

--- vue/src/custom/router/index.ts
+++ vue/src/custom/router/index.ts
@@ -1,5 +1,9 @@
 import { RouteRecordRaw } from 'vue-router'
+import masqueradeRoutes from './masquerade.routes'
 
-const customRoutes: Array<RouteRecordRaw> = []
+const customRoutes: Array<RouteRecordRaw> = [
+    ...masqueradeRoutes
+]
+
 
 export default customRoutes
