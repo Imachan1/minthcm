805a07e45844895d9d01b4b66c81eeb092b9bc17
Michał Nowacki <michal.nowacki@evolpe.pl>
2024-09-27 15:39:15 +0200

    ref #137648 Nie idzie filtrować spotkań po statusach
    ref #137648 Nie idzie filtrować spotkań po statusach

--- vue/src/views/ListView/operators/enum.ts
+++ vue/src/views/ListView/operators/enum.ts
@@ -1,24 +1,36 @@
+
+const _enumModifier = (a) => {
+    let to_lower = (v) => v.toLowerCase()
+
+    if (!Array.isArray(a)) {
+        return to_lower(a)
+    } else {
+        return a.map((v) => v.toLowerCase())
+    }
+
+}
+
 export default {
     equal: {
         label: 'LBL_ESLIST_EQUAL',
-        inputs: [{ type: 'select', label: 'LBL_ESLIST_VALUE' }],
+        inputs: [{ type: 'select', label: 'LBL_ESLIST_VALUE', modifiers: [_enumModifier] }],
         filters: [{ op: 'term', value: '{0}' }],
     },
     not_equal: {
         label: 'LBL_ESLIST_NOT_EQUAL',
         not: true,
-        inputs: [{ type: 'select', label: 'LBL_ESLIST_VALUE' }],
+        inputs: [{ type: 'select', label: 'LBL_ESLIST_VALUE', modifiers: [_enumModifier] }],
         filters: [{ op: 'term', value: '{0}' }],
     },
     contain: {
         label: 'LBL_ESLIST_CONTAIN',
-        inputs: [{ type: 'multiselect', label: 'LBL_ESLIST_VALUES' }],
+        inputs: [{ type: 'multiselect', label: 'LBL_ESLIST_VALUES', modifiers: [_enumModifier] }],
         filters: [{ op: 'terms', value: '{0}' }],
     },
     not_contain: {
         label: 'LBL_ESLIST_NOT_CONTAIN',
         not: true,
-        inputs: [{ type: 'multiselect', label: 'LBL_ESLIST_VALUES' }],
+        inputs: [{ type: 'multiselect', label: 'LBL_ESLIST_VALUES', modifiers: [_enumModifier] }],
         filters: [{ op: 'terms', value: '{0}' }],
     },
 }
