7f0267b43586f12742a566b422faca6a220d93ab
Michał Trudziński <michal.trudzinski@evolpe.pl>
2024-09-27 16:19:25 +0200

    ref #137648 CR

--- vue/src/views/ListView/operators/enum.ts
+++ vue/src/views/ListView/operators/enum.ts
@@ -1,36 +1,26 @@
-
-const _enumModifier = (a) => {
-    let to_lower = (v) => v.toLowerCase()
-
-    if (!Array.isArray(a)) {
-        return to_lower(a)
-    } else {
-        return a.map((v) => v.toLowerCase())
-    }
-
-}
+import { modifiers } from '.'
 
 export default {
     equal: {
         label: 'LBL_ESLIST_EQUAL',
-        inputs: [{ type: 'select', label: 'LBL_ESLIST_VALUE', modifiers: [_enumModifier] }],
+        inputs: [{ type: 'select', label: 'LBL_ESLIST_VALUE', modifiers: [modifiers.toLower] }],
         filters: [{ op: 'term', value: '{0}' }],
     },
     not_equal: {
         label: 'LBL_ESLIST_NOT_EQUAL',
         not: true,
-        inputs: [{ type: 'select', label: 'LBL_ESLIST_VALUE', modifiers: [_enumModifier] }],
+        inputs: [{ type: 'select', label: 'LBL_ESLIST_VALUE', modifiers: [modifiers.toLower] }],
         filters: [{ op: 'term', value: '{0}' }],
     },
     contain: {
         label: 'LBL_ESLIST_CONTAIN',
-        inputs: [{ type: 'multiselect', label: 'LBL_ESLIST_VALUES', modifiers: [_enumModifier] }],
+        inputs: [{ type: 'multiselect', label: 'LBL_ESLIST_VALUES', modifiers: [modifiers.toLower] }],
         filters: [{ op: 'terms', value: '{0}' }],
     },
     not_contain: {
         label: 'LBL_ESLIST_NOT_CONTAIN',
         not: true,
-        inputs: [{ type: 'multiselect', label: 'LBL_ESLIST_VALUES', modifiers: [_enumModifier] }],
+        inputs: [{ type: 'multiselect', label: 'LBL_ESLIST_VALUES', modifiers: [modifiers.toLower] }],
         filters: [{ op: 'terms', value: '{0}' }],
     },
 }
--- vue/src/views/ListView/operators/index.ts
+++ vue/src/views/ListView/operators/index.ts
@@ -27,3 +27,12 @@ export function getAllTypesMatchingTo(baseType: string) {
 
     return [...matchingTypes, baseType]
 }
+
+export const modifiers = {
+    toLower: (a: any): any => {
+        if (!Array.isArray(a)) {
+            return a?.toString().toLowerCase()
+        }
+        return a.map((v) => modifiers.toLower(v))
+    },
+}
