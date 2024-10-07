1cbeb979dce15293fab719a3bd6c32340e09de63
Jakub Kołuda <jakub.koluda@evolpe.pl>
2024-10-07 10:18:55 +0200

    rozszerzenie languages store na potrzeby agenta ai

--- vue/src/store/languages.ts
+++ vue/src/store/languages.ts
@@ -9,6 +9,10 @@ export interface Languages {
     [key: string]: any
 }
 
+interface Placeholders {
+    [key: string]: string
+}
+
 export const useLanguagesStore = defineStore('languages', () => {
     const currentLanguage = 'en_us'
     const languages = ref<Languages>({
@@ -18,13 +22,18 @@ export const useLanguagesStore = defineStore('languages', () => {
     })
 
     const label = computed(() => {
-        return (lbl: string, module?: string) => {
+        return (lbl: string, module?: string | null, placeholders?: Placeholders) => {
             let label = ''
             if (module) {
                 label = languages.value.modules?.[module]?.[lbl]
             }
             if (!label) {
                 label = languages.value.app_strings?.[lbl]
+            }
+            if (placeholders) {
+                for (const [key, value] of Object.entries(placeholders)) {
+                    label = label.replaceAll(`{${key}}`, value)
+                }
             }
             return label || lbl
         }
