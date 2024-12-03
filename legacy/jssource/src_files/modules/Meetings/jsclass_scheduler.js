e9ed1a0ed36e7c295ec346ebcb341248651b1faf
Grzegorz Bauer <grzegorz.bauer@evolpe.pl>
2024-12-03 14:27:07 +0100

    ref #140506 - Spotkania i Rozmowy - Harmonogram uczestników na edycji - sortowanie osób

--- legacy/jssource/src_files/modules/Meetings/jsclass_scheduler.js
+++ legacy/jssource/src_files/modules/Meetings/jsclass_scheduler.js
@@ -865,6 +865,7 @@ SugarWidgetSchedulerAttendees.prototype.display = function () {
    for ( var i = 0; i < GLOBAL_REGISTRY.focus.users_arr.length; i++ ) {
       var row = new SugarWidgetScheduleRow( this.timeslots );
       row.focus_bean = GLOBAL_REGISTRY.focus.users_arr[i];
+      row.data_position = i;
       GLOBAL_REGISTRY.focus.users_arr_hash[ GLOBAL_REGISTRY.focus.users_arr[i]['fields']['id']] = GLOBAL_REGISTRY.focus.users_arr[i];
       // MintHCM #59793 Start
       if ( GLOBAL_REGISTRY.focus.users_arr[i].module === 'Resources' ) {
@@ -961,6 +962,7 @@ SugarWidgetScheduleRow.prototype.display = function () {
          tr = this.thetable.insertRow( this.thetable.rows.length );
       }
       if (tr) {
+        $(tr).attr('data-position', this.data_position);
         tr.className = "schedulerAttendeeRow";
         $( tr ).attr( 'data-id', this.focus_bean.fields.id );
         // MintHCM #54195 #59793 Start
@@ -1006,6 +1008,19 @@ SugarWidgetScheduleRow.prototype.display = function () {
         //}
         this.element = tr;
         this.element_index = this.thetable.rows.length - 1;
+
+        $('#'+this.thetableid).each(function() {
+            let collection = Array.from(this.querySelectorAll('.schedulerAttendeeRow'))
+            .sort(function(x,y) {
+                let posX = +x.dataset.position;
+                let posY = +y.dataset.position;
+                return posX > posY ? 1 : -1;
+            });
+
+            collection.forEach(element => {
+                this.querySelector('tbody').append(element);
+            })
+        });
       }
    }, null, this );
 }
