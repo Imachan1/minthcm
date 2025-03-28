0506750efcea68133ad185ad898f9665e56b88c0
Michał Nowacki <michal.nowacki@evolpe.pl>
2025-03-28 15:46:55 +0100

    michalcommit_1 DD

--- legacy/install/DemoDataInstallation/Services/DemoDataService.php
+++ legacy/install/DemoDataInstallation/Services/DemoDataService.php
@@ -6,8 +6,10 @@ class DemoDataService
 {
     protected $config;
     protected $tables;
+    protected $schema_name;
 
     const CONFIG_FILE_PATH = 'install/DemoDataInstallation/Configs/demo_data.php';
+    const MINT_CONFIG_FILE_PATH = 'config.php';
     const SQL_FILES_PATH = 'install/demo_data';
 
     public function __construct()
@@ -25,9 +27,15 @@ class DemoDataService
         return $this->tables;
     }
 
+    public function getSchemaName() {
+        return $this->schema_name;
+    }
     protected function loadConfig(){
         require static::CONFIG_FILE_PATH;
         $this->config = $config;
+
+        require static::CONFIG_FILE_PATH;
+        $this->schema_name = $sugar_config['dbconfig']['db_name'];
     }
 
     protected function loadTablesWithSQLFilesInfo() {
