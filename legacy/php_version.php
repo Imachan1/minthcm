391ef92643831ac3d838c8b9834b8a3eee134fca
Michał Nowacki <michal.nowacki@evolpe.pl>
2025-04-28 20:31:32 +0200

    Poprawa fatala podczas instalacji

--- legacy/php_version.php
+++ legacy/php_version.php
@@ -9,5 +9,5 @@ define('SUITECRM_PHP_MIN_VERSION', '7.4.0');
 // The minimum recommended version on which to install SuiteCRM
 define('SUITECRM_PHP_REC_VERSION', '7.4.0');
 
-define('MINTHCM_PHP_MAX_VERSION', '8.0.99');
+define('MINTHCM_PHP_MAX_VERSION', '8.2.99');
 
