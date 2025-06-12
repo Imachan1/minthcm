43d93ed618ed6c1110e20dfe1558adeb78f72ded
Grzegorz Bauer <grzegorz.bauer@evolpe.pl>
2025-06-12 11:04:13 +0200

    ref #155276 Dostosować widok listy pracowników #CR

--- vue/src/views/ListView/ListViewStore.ts
+++ vue/src/views/ListView/ListViewStore.ts
@@ -122,7 +122,7 @@ export const useListViewStore = defineStore('listview', () => {
 
     function setDefaultColumns() {
         if (preferences.value?.columns) {
-            preferences.value.columns = null
+            preferences.value.columns = []
         }
     }
 
