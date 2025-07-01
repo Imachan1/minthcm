ac3dbf6f03d97745e0f1c412523aacd51c15ba00
Ahmed Osman <ahmed.osman#evolpe.pl>
2025-07-01 13:57:50 +0200

    ref #155587 vendor

---
+++ mcp/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'cc4039a50d1b2fac2344ff11b0f834d6536092fb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'cc4039a50d1b2fac2344ff11b0f834d6536092fb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'logiscape/mcp-sdk-php' => array(
+            'pretty_version' => 'v1.2.1',
+            'version' => '1.2.1.0',
+            'reference' => 'aaab1ecc0a1c1e0cecf4c09a916b2cea8c638b71',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../logiscape/mcp-sdk-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/log' => array(
+            'pretty_version' => '3.0.2',
+            'version' => '3.0.2.0',
+            'reference' => 'f16e1d5863e37f8d8c2a01719f5b34baa2b714d3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/log',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
