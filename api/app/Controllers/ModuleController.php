6a54b554765c9c4eb27bfcd1bbcb5c78f2b7b722
Michał Nowacki <michal.nowacki@evolpe.pl>
2025-07-16 10:21:14 +0200

    podstawowe poprawki do rekordów
    (cherry picked from commit 7de7c412494f64cfe936371dd0a5a48fd218181a)

--- api/app/Controllers/ModuleController.php
+++ api/app/Controllers/ModuleController.php
@@ -274,7 +274,7 @@ class ModuleController
             
             $target_module = $spd->layout_defs['subpanel_setup'][$related_name]['module'];
             $target_bean = BeanFactory::getBean($target_module);
-            if (!$target_bean->ACLAccess('list')) {
+            if (!$target_bean || !$target_bean->ACLAccess('list')) {
                 return $response->withStatus(403);
             }
 
