5285e2a29084944cfd5e0fd5657c39a6a517982d
Jakub Kołuda <jakub.koluda@evolpe.pl>
2025-07-21 12:16:06 +0200

    ref #144598 Filtr pola relacyjnego - dodanie fieldsetu do widoku Kandydatów, poprawka na froncie

--- vue/src/components/MintPanel/MintPanelRecordDetails/MintPanelRecordDetails.vue
+++ vue/src/components/MintPanel/MintPanelRecordDetails/MintPanelRecordDetails.vue
@@ -32,7 +32,7 @@
             </div>
         </div>
         <div class="fields-container">
-            <div v-for="(row, i) in fixedRows" class="row" :key="row">
+            <div v-for="(row, i) in rows" class="row" :key="row">
                 <div v-for="n in store.columns" :key="n - 1">
                     <Field
                         v-if="row[n - 1] && !store.bean.logic.hiddenFields.includes(row[n - 1].name)"
@@ -81,23 +81,8 @@ const title = computed(() => {
     return languages.label(props.data?.title ?? 'LBL_DETAILS', modules.currentModule?.name)
 })
 
-const fixedRows = computed(() => {
-    const rows = props.data.fields.filter((row) => row.some((field) => !store.bean.logic.hiddenFields.includes(field.name))) || []
-    const fixedRows: FieldVardef[][] = []
-    rows.forEach((row) => {
-        const newRow = []
-        row.forEach((field) => {
-            if (field.type === 'fieldset' && field.properties?.fields && store.view === 'edit') {
-                newRow.push(...field.properties.fields)
-            } else {
-                newRow.push(field)
-            }
-        })
-        while (newRow.length) {
-            fixedRows.push(newRow.splice(0, store.columns))
-        }
-    })
-    return fixedRows
+const rows = computed(() => {
+    return props.data.fields.filter((row) => row.some((field) => !store.bean.logic.hiddenFields.includes(field.name)))
 })
 
 const inlineEditBtnClicked = (event: string) => {
