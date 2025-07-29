ce8d09f1df07be4ba2ac809ef67936f6b3298c2a
Grzegorz Bauer <grzegorz.bauer@evolpe.pl>
2025-07-29 11:35:19 +0200

    ref #133686 - Zamykanie spotkań z poziomu subpanelu - poprawki BUG 2

--- legacy/modules/Reservations/logic_hooks/ReservationsLogicHook.php
+++ legacy/modules/Reservations/logic_hooks/ReservationsLogicHook.php
@@ -67,7 +67,7 @@ class ReservationsLogicHook {
          $rel = $this->resources_rel;
          if ( $bean->load_relationship($rel) ) {
             $resources_ids = $bean->$rel->get();
-            if(!empty($resources_ids)) {
+            if(!empty($resources_ids) && !empty($bean->resources_arr)) {
                 $this->createReservations($bean, array_diff($bean->resources_arr, $resources_ids));
             }
             if ( $bean->date_start != $bean->fetched_row['date_start'] ||
