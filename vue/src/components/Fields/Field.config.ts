cde1fb3e6fd6feb46b560a10f640df674b090af7
Michał Nowacki <michal.nowacki@evolpe.pl>
2025-08-29 14:43:17 +0200

    ref #164817 Pole Wauta - Wyświetla się jako varchar
    ref #164817 Pole Wauta - Wyświetla się jako varchar
    
    ref #164817 Pole Wauta - Wyświetla się jako varchar

--- vue/src/components/Fields/Field.config.ts
+++ vue/src/components/Fields/Field.config.ts
@@ -6,27 +6,27 @@ const fieldsOptions = import.meta.glob('@/components/Fields/*/*options.ts', { ea
 const getFieldName = (path: string) => path.split('/').slice(-2, -1)[0]
 
 export const fieldConfig = {
-  allowedTypes: {
-    list: Object.keys(listFields).map(getFieldName),
-    edit: Object.keys(editFields).map(getFieldName),
-    detail: Object.keys(detailFields).map(getFieldName),
-  },
+    allowedTypes: {
+        list: Object.keys(listFields).map(getFieldName),
+        edit: Object.keys(editFields).map(getFieldName),
+        detail: Object.keys(detailFields).map(getFieldName),
+    },
 
-  options: Object.fromEntries(
-    Object.entries(fieldsOptions).map(([path, module]) => {
-      const name = getFieldName(path)
-      const m = module as { default: any }
-      return [name, m.default ?? m]
-    }),
-  ),
+    options: Object.fromEntries(
+        Object.entries(fieldsOptions).map(([path, module]) => {
+            const name = getFieldName(path)
+            const m = module as { default: any }
+            return [name, m.default ?? m]
+        }),
+    ),
 
-  defaultType: 'varchar',
+    defaultType: 'varchar',
 
-  typeMap: {
-    char: 'varchar',
-    datetimecombo: 'datetime',
-    ColoredActivityStatus: 'enum',
-    ColoredEnum: 'enum',
-    image: 'file',
-  } as { [key: string]: string },
+    typeMap: {
+        char: 'varchar',
+        datetimecombo: 'datetime',
+        ColoredActivityStatus: 'enum',
+        ColoredEnum: 'enum',
+        image: 'file',
+    } as { [key: string]: string },
 }
