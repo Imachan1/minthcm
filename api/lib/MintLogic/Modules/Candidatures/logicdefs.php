7c0b530668da11e1648d2f1a1b9d3eedd72c6a06
Michał Trudziński <michal.trudzinski@gmail.com>
2025-09-22 12:33:55 +0000

    ref #164775 BUG: Czyszczenie się pola Date

--- api/lib/MintLogic/Modules/Candidatures/logicdefs.php
+++ api/lib/MintLogic/Modules/Candidatures/logicdefs.php
@@ -5,34 +5,24 @@ use MintHCM\Lib\MintLogic\Hook;
 
 return [
     'rules' => [
-        'init' => [
-            'hooks' => [Hook::ALL],
+        'hired' => [
+            'hooks' => [Hook::INIT, Hook::CHANGE],
+            'triggerFields' => ['status'],
+            'trigger' => Formula::notInArray('$status', ['Acceptance', 'Hired']),
             'logic' => [
                 'visible' => [
                     'work_start' => false,
                     'training_date' => false,
-                    'reason_for_rejection' => false,
-                ],
-            ],
-        ],
-        'hired' => [
-            'hooks' => [Hook::ALL, Hook::CHANGE],
-            'triggerFields' => ['status'],
-            'trigger' => Formula::inArray('$status', ['Acceptance', 'Hired']),
-            'logic' => [
-                'visible' => [
-                    'work_start' => true,
-                    'training_date' => true,
                 ],
             ],
         ],
         'rejection' => [
-            'hooks' => [Hook::ALL, Hook::CHANGE],
+            'hooks' => [Hook::INIT, Hook::CHANGE],
             'triggerFields' => ['status'],
-            'trigger' => Formula::inArray('$status', ['Rejected']),
+            'trigger' => Formula::notInArray('$status', ['Rejected']),
             'logic' => [
                 'visible' => [
-                    'reason_for_rejection' => true,
+                    'reason_for_rejection' => false,
                 ],
             ],
         ],
