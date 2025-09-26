ed0814789c078735110eac435a6b43a30d273f58
Michał Nowacki <michal.nowacki@evolpe.pl>
2025-09-26 09:24:55 +0200

    ref #158443 dostosowanie pod wersje 430

--- vue/src/api/favorites.api.ts
+++ vue/src/api/favorites.api.ts
@@ -1,22 +1,22 @@
-import { MintApi } from './api'
+import { mintApi } from './api'
 
-class FavoritesApi extends MintApi {
+class FavoritesApi {
     public async add(module: string, id: string) {
-        return await this.instance.post('/favorites/add', {
+        return await mintApi.post('/favorites/add', {
             module: module,
             id: id,
         })
     }
 
     public async remove(module: string, id: string) {
-        return await this.instance.post('/favorites/remove', {
+        return await mintApi.post('/favorites/remove', {
             module: module,
             id: id,
         })
     }
 
     public async getList() {
-        return await this.instance.get('/Favorites')
+        return await mintApi.get('/Favorites')
     }
 }
 
