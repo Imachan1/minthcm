b7827fd580bb9e2c89c659d59c225091ae9584bc
Michał Nowacki <michal.nowacki@evolpe.pl>
2025-10-10 10:51:30 +0200

    ref #170179 Globalny problem z niedziałającymi opcjami/filtrami na dashletach

--- legacy/include/language/en_us.lang.php
+++ legacy/include/language/en_us.lang.php
@@ -1793,6 +1793,7 @@ $app_strings = array(
     'LBL_SAVED_LAYOUT' => 'Layout has been saved.',
     'LBL_SAVED' => 'Saved',
     'LBL_SAVING' => 'Saving',
+    'LBL_VALIDATION' => 'Validation in progress...',
     'LBL_DISPLAY_COLUMNS' => 'Display Columns',
     'LBL_HIDE_COLUMNS' => 'Hide Columns',
     'LBL_PROCESSING_REQUEST' => 'Processing...',
