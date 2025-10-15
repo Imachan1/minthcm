1d84790ac4e41ac03cfb60dbebdc45a6e7b2832c
Dominik Sucharski <dominik.sucharski@evolpe.pl>
2025-10-15 12:08:52 +0200

    ref #170401 Brak możliwości zalogowania - Mint 4.3.0

--- legacy/Api/V8/OAuth2/Grant/MobileGrant.php
+++ legacy/Api/V8/OAuth2/Grant/MobileGrant.php
@@ -1,11 +1,15 @@
 <?php
-
 namespace Api\V8\OAuth2\Grant;
 
-use BeanFactory; // MintHCM #136592
-use League\OAuth2\Server\Entities\ClientEntityInterface; // MintHCM #136592
-use League\OAuth2\Server\Grant\PasswordGrant;
-use Psr\Http\Message\ServerRequestInterface;  // MintHCM #131001
+use BeanFactory;  // MintHCM #136592
+use DateInterval; // MintHCM #136592
+use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Grant\PasswordGrant; // MintHCM #131001
+use League\OAuth2\Server\RequestAccessTokenEvent;
+use League\OAuth2\Server\RequestEvent;
+use League\OAuth2\Server\RequestRefreshTokenEvent;
+use League\OAuth2\Server\ResponseTypes\ResponseTypeInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 #[\AllowDynamicProperties]
 class MobileGrant extends PasswordGrant
@@ -46,4 +50,33 @@ class MobileGrant extends PasswordGrant
         return $user;
     }
     // MintHCM #136592 end
+
+    public function respondToAccessTokenRequest(
+        ServerRequestInterface $request,
+        ResponseTypeInterface $responseType,
+        DateInterval $accessTokenTTL
+    ) {
+        // Validate request
+        $client = $this->validateClient($request);
+        $scopes = $this->validateScopes($this->getRequestParameter('scope', $request, $this->defaultScope));
+        $user = $this->validateUser($request, $client);
+
+        // Finalize the requested scopes
+        $finalizedScopes = $this->scopeRepository->finalizeScopes($scopes, $this->getIdentifier(), $client, $user->getIdentifier()); // MintHCM #170401
+
+        // Issue and persist new access token
+        $accessToken = $this->issueAccessToken($accessTokenTTL, $client, $user->getIdentifier(), $finalizedScopes); // MintHCM #170401
+        $this->getEmitter()->emit(new RequestAccessTokenEvent(RequestEvent::ACCESS_TOKEN_ISSUED, $request, $accessToken));
+        $responseType->setAccessToken($accessToken);
+
+        // Issue and persist new refresh token if given
+        $refreshToken = $this->issueRefreshToken($accessToken);
+
+        if ($refreshToken !== null) {
+            $this->getEmitter()->emit(new RequestRefreshTokenEvent(RequestEvent::REFRESH_TOKEN_ISSUED, $request, $refreshToken));
+            $responseType->setRefreshToken($refreshToken);
+        }
+
+        return $responseType;
+    }
 }
