b039ac4233eb6c80fd50a303e138f5f37da6a1f0
Grzegorz Bauer <grzegorz.bauer@evolpe.pl>
2025-11-13 16:10:20 +0100

    ref #174286 Chowanie i ukrywanie menu #CR

--- vue/src/layouts/DefaultLayout/DefaultLayoutSidebar.vue
+++ vue/src/layouts/DefaultLayout/DefaultLayoutSidebar.vue
@@ -1,155 +1,164 @@
 <template>
-    <v-navigation-drawer
-        class="sidebar-nav"
-        expand-on-hover
-        :rail="$vuetify.display.mdAndDown"
-        permanent
-        width="260"
-        :color="$vuetify.display.mdAndDown ? '#d5e6e4dd' : '#00000010'"
-        :floating="$vuetify.display.lgAndUp"
-        rail-width="76"
-    >
-        <v-list
-            v-if="modules.currentModule?.actions"
-            nav
-            bg-color="primary"
-            class="nav-list flex-shrink-0 py-4"
+    <div class="default-layout-sidebar">
+        <v-navigation-drawer
+            class="sidebar-nav"
+            expand-on-hover
+            :rail="$vuetify.display.mdAndDown || shrinked"
+            permanent
+            width="260"
+            color="#00000010"
+            floating="true"
+            rail-width="76"
         >
-            <v-list-item
-                v-for="action in modules.currentModule.actions"
-                :key="action.action+modules.currentModule"
-                class="nav-item module-action"
-                :value="action.action"
-                :to="action.url ? url.fromLegacyUrl(action.url) : ''"
-                :active="false"
+            <v-list
+                v-if="modules.currentModule?.actions"
+                nav
+                bg-color="primary"
+                class="nav-list flex-shrink-0 py-4"
             >
-                <div class="nav-title">
-                    <v-icon :icon="`mdi-${action.icon}`" />
-                    <span v-text="action.name" />
-                </div>
-            </v-list-item>
-        </v-list>
-        <div class="flex-grow-1" style="display: flex; flex-direction: column; overflow: auto">
-            <v-text-field
-                v-model="filterModulesQuery"
-                class="find-module"
-                :placeholder="languages.label('LBL_MINT4_FIND_MODULE')"
-                variant="plain"
-                density="compact"
-                hide-details
-            >
-                <template #prepend-inner>
-                    <v-fab-transition>
-                        <v-icon v-if="filterModulesQuery" icon="mdi-close" @click="clearInput" />
-                        <v-icon v-else icon="mdi-magnify" />
-                    </v-fab-transition>
-                </template>
-            </v-text-field>
-            <v-list nav class="nav-list nav-list-blurred flex-grow-1" style="min-height: 80px">
-                <transition-group name="list" tag="ul">
-                    <template v-if="filteredModules.length">
-                        <v-list-item
-                            class="nav-item"
-                            v-for="filteredModule in filteredModules"
-                            :key="filteredModule.name"
-                            :value="filteredModule.name"
-                            :data-cy="filteredModule.name"
-                            :to="
-                                ![
-                                    'Calls',
-                                    'Candidates',
-                                    'Meetings',
-                                    'Tasks',
-                                    'Candidatures',
-                                    'Positions',
-                                    'Recruitments',
-                                ].includes(filteredModule.name)
-                                    ? `/modules/${filteredModule.name}`
-                                    : `/modules/${filteredModule.name}/ESListView`
-                            "
-                            :active="filteredModule.name === url.module"
-                            color="secondary"
-                        >
-                            <div style="display: flex; align-items: center; justify-content: space-between">
-                                <div class="nav-title">
-                                    <v-icon :icon="`${filteredModule.icon}`" />
-                                    <span v-text="filteredModule.label" />
-                                </div>
-                                <v-menu v-if="filteredModule.name !== 'Home' && filteredModule.actions?.length">
-                                    <template v-slot:activator="{ props, isActive }">
-                                        <v-btn
-                                            v-bind="props"
-                                            @click.prevent.stop="null"
-                                            class="menu-icon"
-                                            :class="[isActive && 'menu-icon-active']"
-                                            icon="mdi-dots-vertical"
-                                            variant="text"
-                                            density="compact"
-                                            color="secondary"
-                                        />
-                                    </template>
-                                    <MintMenuList :items="parseModuleActions(filteredModule.actions)" />
-                                </v-menu>
-                            </div>
-                        </v-list-item>
-                    </template>
-                    <span
-                        v-else
-                        class="px-4 text-grey-darken-4"
-                        v-text="languages.label('LBL_MINT4_NO_MODULES_FOUND')"
-                    />
-                </transition-group>
+                <v-list-item
+                    v-for="action in modules.currentModule.actions"
+                    :key="action.action+modules.currentModule"
+                    class="nav-item module-action"
+                    :value="action.action"
+                    :to="action.url ? url.fromLegacyUrl(action.url) : ''"
+                    :active="false"
+                >
+                    <div class="nav-title">
+                        <v-icon :icon="`mdi-${action.icon}`" />
+                        <span v-text="action.name" />
+                    </div>
+                </v-list-item>
             </v-list>
-            <v-expansion-panels class="nav-accordion" variant="accordion">
-                <v-expansion-panel v-if="recents.recents?.length" bg-color="transparent">
-                    <v-expansion-panel-title>
-                        <v-icon class="mr-4" icon="mdi-history" />
-                        <span v-text="languages.label('LBL_MINT4_RECENTLY_VIEWED')" />
-                    </v-expansion-panel-title>
-                    <v-expansion-panel-text>
-                        <v-list nav class="nav-list">
+            <div class="flex-grow-1" style="display: flex; flex-direction: column; overflow: auto">
+                <v-text-field
+                    v-model="filterModulesQuery"
+                    class="find-module"
+                    :placeholder="languages.label('LBL_MINT4_FIND_MODULE')"
+                    variant="plain"
+                    density="compact"
+                    hide-details
+                >
+                    <template #prepend-inner>
+                        <v-fab-transition>
+                            <v-icon v-if="filterModulesQuery" icon="mdi-close" @click="clearInput" />
+                            <v-icon v-else icon="mdi-magnify" />
+                        </v-fab-transition>
+                    </template>
+                </v-text-field>
+                <v-list nav class="nav-list nav-list-blurred flex-grow-1" style="min-height: 80px">
+                    <transition-group name="list" tag="ul">
+                        <template v-if="filteredModules.length">
                             <v-list-item
-                                v-for="recent in recents.recents?.slice(0, 5) ?? []"
-                                :key="recent.item_id"
                                 class="nav-item"
-                                :value="recent.item_id"
-                                :to="`/modules/${recent.module_name}/DetailView/${recent.item_id}`"
-                                :active="false"
+                                v-for="filteredModule in filteredModules"
+                                :key="filteredModule.name"
+                                :value="filteredModule.name"
+                                :data-cy="filteredModule.name"
+                                :to="
+                                    ![
+                                        'Calls',
+                                        'Candidates',
+                                        'Meetings',
+                                        'Tasks',
+                                        'Candidatures',
+                                        'Positions',
+                                        'Recruitments',
+                                    ].includes(filteredModule.name)
+                                        ? `/modules/${filteredModule.name}`
+                                        : `/modules/${filteredModule.name}/ESListView`
+                                "
+                                :active="filteredModule.name === url.module"
+                                color="secondary"
                             >
-                                <div class="nav-title">
-                                    <v-icon :icon="modules.modules[recent.module_name]?.icon ?? 'mdi-clock'" />
-                                    <span v-text="recent.item_summary" />
+                                <div style="display: flex; align-items: center; justify-content: space-between">
+                                    <div class="nav-title">
+                                        <v-icon :icon="`${filteredModule.icon}`" />
+                                        <span v-text="filteredModule.label" />
+                                    </div>
+                                    <v-menu v-if="filteredModule.name !== 'Home' && filteredModule.actions?.length">
+                                        <template v-slot:activator="{ props, isActive }">
+                                            <v-btn
+                                                v-bind="props"
+                                                @click.prevent.stop="null"
+                                                class="menu-icon"
+                                                :class="[isActive && 'menu-icon-active']"
+                                                icon="mdi-dots-vertical"
+                                                variant="text"
+                                                density="compact"
+                                                color="secondary"
+                                            />
+                                        </template>
+                                        <MintMenuList :items="parseModuleActions(filteredModule.actions)" />
+                                    </v-menu>
                                 </div>
                             </v-list-item>
-                        </v-list>
-                    </v-expansion-panel-text>
-                </v-expansion-panel>
-                <v-expansion-panel v-if="favorites.favorites?.length" bg-color="transparent" elevetion="10">
-                    <v-expansion-panel-title>
-                        <v-icon class="mr-4" icon="mdi-heart" />
-                        <span v-text="languages.label('LBL_MINT4_FAVORITE_RECORDS')" />
-                    </v-expansion-panel-title>
-                    <v-expansion-panel-text>
-                        <v-list nav class="nav-list">
-                            <v-list-item
-                                v-for="favorite in favorites.favorites?.slice(0, 5) ?? []"
-                                :key="favorite.id"
-                                class="nav-item"
-                                :value="favorite.id"
-                                :to="`/modules/${favorite.module_name}/DetailView/${favorite.id}`"
-                                :active="false"
-                            >
-                                <div class="nav-title">
-                                    <v-icon :icon="modules.modules[favorite.module_name]?.icon ?? 'mdi-heart'" />
-                                    <span v-text="favorite.item_summary" />
-                                </div>
-                            </v-list-item>
-                        </v-list>
-                    </v-expansion-panel-text>
-                </v-expansion-panel>
-            </v-expansion-panels>
+                        </template>
+                        <span
+                            v-else
+                            class="px-4 text-grey-darken-4"
+                            v-text="languages.label('LBL_MINT4_NO_MODULES_FOUND')"
+                        />
+                    </transition-group>
+                </v-list>
+                <v-expansion-panels class="nav-accordion" variant="accordion">
+                    <v-expansion-panel v-if="recents.recents?.length" bg-color="transparent">
+                        <v-expansion-panel-title>
+                            <v-icon class="mr-4" icon="mdi-history" />
+                            <span v-text="languages.label('LBL_MINT4_RECENTLY_VIEWED')" />
+                        </v-expansion-panel-title>
+                        <v-expansion-panel-text>
+                            <v-list nav class="nav-list">
+                                <v-list-item
+                                    v-for="recent in recents.recents?.slice(0, 5) ?? []"
+                                    :key="recent.item_id"
+                                    class="nav-item"
+                                    :value="recent.item_id"
+                                    :to="`/modules/${recent.module_name}/DetailView/${recent.item_id}`"
+                                    :active="false"
+                                >
+                                    <div class="nav-title">
+                                        <v-icon :icon="modules.modules[recent.module_name]?.icon ?? 'mdi-clock'" />
+                                        <span v-text="recent.item_summary" />
+                                    </div>
+                                </v-list-item>
+                            </v-list>
+                        </v-expansion-panel-text>
+                    </v-expansion-panel>
+                    <v-expansion-panel v-if="favorites.favorites?.length" bg-color="transparent" elevetion="10">
+                        <v-expansion-panel-title>
+                            <v-icon class="mr-4" icon="mdi-heart" />
+                            <span v-text="languages.label('LBL_MINT4_FAVORITE_RECORDS')" />
+                        </v-expansion-panel-title>
+                        <v-expansion-panel-text>
+                            <v-list nav class="nav-list">
+                                <v-list-item
+                                    v-for="favorite in favorites.favorites?.slice(0, 5) ?? []"
+                                    :key="favorite.id"
+                                    class="nav-item"
+                                    :value="favorite.id"
+                                    :to="`/modules/${favorite.module_name}/DetailView/${favorite.id}`"
+                                    :active="false"
+                                >
+                                    <div class="nav-title">
+                                        <v-icon :icon="modules.modules[favorite.module_name]?.icon ?? 'mdi-heart'" />
+                                        <span v-text="favorite.item_summary" />
+                                    </div>
+                                </v-list-item>
+                            </v-list>
+                        </v-expansion-panel-text>
+                    </v-expansion-panel>
+                </v-expansion-panels>
+            </div>
+        </v-navigation-drawer>
+        <div 
+            class="shrinker" 
+            :class="{ 'rail-mode': shrinked }"
+            v-if="!$vuetify.display.mdAndDown"
+        >
+            <MintButton variant="nav" :icon="shrinked || $vuetify.display.mdAndDown ? 'mdi-chevron-right' : 'mdi-chevron-left'" @click="shrink" />
         </div>
-    </v-navigation-drawer>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -160,12 +169,19 @@ import { useRecentsStore } from '@/store/recents'
 import { useModulesStore, ModuleAction } from '@/store/modules'
 import MintMenuList from '@/components/MintMenuList.vue'
 import { useLanguagesStore } from '@/store/languages'
+import MintButton from '@/components/MintButtons/MintButton.vue'
 
 const modules = useModulesStore()
 const url = useUrlStore()
 const favorites = useFavoritesStore()
 const recents = useRecentsStore()
 const languages = useLanguagesStore()
+
+const shrinked = ref(false)
+
+const shrink = () => {
+    shrinked.value = !shrinked.value
+}
 
 const filterModulesQuery = ref('')
 const filteredModules = computed(() => {
@@ -339,6 +355,71 @@ function clearInput() {
     }
     .v-icon {
         opacity: 1;
+    }
+}
+
+.shrinker {
+    position: fixed;
+    top: 50%;
+    left: calc(260px - 52px);
+    transform: translateY(-50%);
+    width: 52px;
+    height: 52px;
+    background: rgb(var(--v-theme-surface));;
+    border-radius: 0 50% 50% 0;
+    z-index: 1001;
+    cursor: pointer;
+    transition: left 0.1s ease;
+    display: flex;
+    visibility: hidden;
+    
+    
+    &.rail-mode {
+        left: calc(76px - 52px);
+    }
+
+    align-items: center;
+    justify-content: center;
+
+    box-shadow: 0px 1px 6px #00000029;
+    &::before,
+    &::after {
+        content: '';
+        position: absolute;
+        width: 26px;
+        height: 26px;
+        background-color: rgb(var(--v-theme-surface));
+        left: 0px;
+        border-radius: inherit;
+        transition: width 0.3s ease;
+        mask: radial-gradient(circle at center, transparent 13px, black 0%);
+        -webkit-mask: radial-gradient(circle at center, transparent 13px, black 0%);
+    }
+    &::before {
+        top: -26px;
+        border-radius: 50% 50% 50% 0px;
+
+    }
+    &::after {
+        bottom: -26px;
+        border-radius: 0px 50% 50% 50%;
+    }
+}
+
+.default-layout-sidebar {
+    &:hover {
+        .shrinker {
+            left: calc(260px);
+            visibility: visible;
+        }
+    }
+
+    &:hover,
+    &:has(.shrinker:hover) {
+        .v-navigation-drawer--rail {
+            transform: translateX(0) !important;
+            width: 260px !important;
+        }
     }
 }
 </style>
