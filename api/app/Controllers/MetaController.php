887d8c33f387929e05b85439d010570b655ee2f4
Grzegorz Bauer <grzegorz.bauer@evolpe.pl>
2025-11-17 15:24:37 +0100

    ref #174650 Dodawanie podstawowych akcji do widoków RecordView #CR

--- api/app/Controllers/MetaController.php
+++ api/app/Controllers/MetaController.php
@@ -20,6 +20,12 @@ class MetaController
         'date_modified',
     ];
 
+    protected const DEFAULT_ACTIONS = [
+        'Audit',
+        'Delete',
+        'Duplicate',
+    ];
+
     public function getEditViewMeta($module)
     {
         chdir('../legacy/');
@@ -88,6 +94,10 @@ class MetaController
                         'collapsed' => true,
                     ];
                     $data['recordview']['panels'][$panel]['data']['sections']['other']['fields'] = $this->mergeModuleFields($views[$panel]['other'],$module_fields)['fields'];
+                }
+                
+                if (!isset($panel_defs['data']['actions'])) {
+                    $data['recordview']['panels'][$panel]['data']['actions'] = self::DEFAULT_ACTIONS;
                 }
 
                 continue;
