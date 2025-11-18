edf990a1f057952f1e1ac56c92c5bf6b76ecc564
Szymon Maćkowiak <szymon.mackowiak@evolpe.pl>
2025-11-18 11:36:59 +0100

    ref #167711 Dodanie nowego Typu dostępu dla Ról #CR|michal.nowacki

--- legacy/modules/ACLActions/actiondefs.php
+++ legacy/modules/ACLActions/actiondefs.php
@@ -11,7 +11,7 @@ if (!defined('sugarEntry') || !sugarEntry) {
  * Copyright (C) 2011 - 2018 SalesAgility Ltd.
  *
  * MintHCM is a Human Capital Management software based on SuiteCRM developed by MintHCM, 
- * Copyright (C) 2018-2023 MintHCM
+ * Copyright (C) 2018-2024 MintHCM
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Affero General Public License version 3 as published by the
@@ -90,7 +90,7 @@ $GLOBALS['ACLActions'] = array(
                                 array(
                                     'aclaccess'=>array(ACL_ALLOW_DEFAULT,ACL_ALLOW_NORMAL,ACL_ALLOW_ADMIN),
                                     'label'=>'LBL_ACTION_ADMIN',
-                                    'default'=>ACL_ALLOW_ADMIN,
+                                    'default'=>ACL_ALLOW_NORMAL,
                                 ),
                         'view'=>
                                 array(
