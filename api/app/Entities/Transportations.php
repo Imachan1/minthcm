b98a242619bf2e8fe05a3d5f19ba8ce60e89fe76
Michał Nowacki <michal.nowacki@evolpe.pl>
2025-12-19 08:59:39 +0100

    syntax na encjach

--- api/app/Entities/Transportations.php
+++ api/app/Entities/Transportations.php
@@ -172,11 +172,6 @@ class Transportations extends MintEntity
     protected $delegation_id;
 
     /**
-     * @ORM\Column(type="string", length="36")
-     */
-    public $delegation_id;
-
-    /**
      * @ORM\JoinColumn(name="modified_user_id", referencedColumnName="id")
      * @ORM\ManyToOne(targetEntity=Users::class, inversedBy="transportations")
      */
